ee7eddd35d736660cefcaef0de1fa7cd3a761294
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-11 23:34:27 -0500

    Launched the the rest server in an other thread

--- Server/cmd/server/main.go
+++ Server/cmd/server/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"log"
+
 	"gitlab.com/jigsawcorp/log3900/internal/rest"
 	"gitlab.com/jigsawcorp/log3900/pkg/graceful"
 )
 
 var restServer *rest.RestServer
+
 func main() {
 	graceful.CatchSigterm(onSIGTERM)
 
 	restServer = &rest.RestServer{}
-	restServer.Initialize()
-	restServer.Run(":3000")
+	hRestServer := make(chan bool)
+	go func(){
+		restServer.Initialize()
+		restServer.Run(":3000")
+		hRestServer <- true
+	}()
+	
+	log.Printf("REST API server launched!")
+
+	
+	//TODO Launch other servers and handles
+	
+
+	<-hRestServer
 }
 
 // Call this function on crtl+c
