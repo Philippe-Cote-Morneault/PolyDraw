28a8c8933fb7756be7de565daf5cbb566b67db67
cedric <cedric.tessier@polymtl.ca>
2020-01-15 23:30:21 -0500

    added a hamburger menu to the mainwindow

---
+++ Desktop/Projet3/ClientLourd/Views/Chat.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ClientLourd.Views
+{
+    public partial class Chat : UserControl
+    {
+        public Chat()
+        {
+            InitializeComponent();
+        }
+    }
+}
