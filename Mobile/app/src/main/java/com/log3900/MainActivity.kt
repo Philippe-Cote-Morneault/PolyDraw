51c9e9c33f039505bd58e365ce0aee41cc9f36d3
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-01-16 18:26:51 -0500

    Added basic login page UI

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.log3900
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.log3900.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /* TODO: token validation */
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ Mobile/app/src/main/java/com/log3900/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.log3900.login
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.log3900.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login) // TODO Login layout with material design
+    }
+}
