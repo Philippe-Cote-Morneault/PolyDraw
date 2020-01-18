ff1c7fb0e33c15ef6d56dc813036f82cb7b93f74
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-18 14:08:17 -0500

    Added unsubsribe method.

--- Server/internal/socket/publicInterface.go
+++ Server/internal/socket/publicInterface.go
@@ -5,11 +5,23 @@ import (
 	"github.com/vmihailenco/msgpack/v4"
 )
 
-func (manager *ClientSocketManager) Subscribe(messageType int, callback SocketCallback) {
+// The passed callback will be called when messageType event is received. Returns a uuid used to unsubscribe
+func (manager *ClientSocketManager) Subscribe(messageType int, callback SocketCallback) uuid.UUID {
 	if _, ok := manager.subscribers[messageType]; !ok {
-		manager.subscribers[messageType] = []SocketCallback{}
+		manager.subscribers[messageType] = make(map[uuid.UUID]SocketCallback)
 	}
-	manager.subscribers[messageType] = append(manager.subscribers[messageType], callback)
+
+	callbackId := uuid.New()
+	manager.subscribers[messageType][callbackId] = callback
+
+	return callbackId
+}
+
+func (manager *ClientSocketManager) Unsubscribe(messageType int, callbackId uuid.UUID) {
+	if _, ok := manager.subscribers[messageType]; ok {
+		callbacks := manager.subscribers[messageType]
+		delete(callbacks, callbackId)
+	}
 }
 
 // Sends a SocketMessage to the socketId
@@ -39,6 +51,7 @@ func (manager *ClientSocketManager) SendRawMessageToSocketId(message RawMessage,
 	return nil
 }
 
+// TODO: Implement when we have user service
 func (manager *ClientSocketManager) SendMessageToUsername(message SocketMessage, username string) {
 
 }
