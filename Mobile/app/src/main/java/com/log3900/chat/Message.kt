246164d1c1532593641a56d8a27f066926535d9d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-19 17:38:34 -0500

    Added Message data class.

---
+++ Mobile/app/src/main/java/com/log3900/chat/Message.kt
@@ -0,0 +1,6 @@
+package com.log3900.chat
+
+import java.sql.Time
+import java.util.*
+
+data class Message(var message: String, var channelID:  UUID, var senderID: UUID, var senderName: String, var timestamp: Time)
