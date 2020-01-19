775650d4e349946868ef5ae04aa0b999a4527b79
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-19 17:45:18 -0500

    Added initialize and receiveMessage function signatures.

--- Mobile/app/src/main/java/com/log3900/chat/MessageService.kt
+++ Mobile/app/src/main/java/com/log3900/chat/MessageService.kt
@@ -5,4 +5,12 @@ class MessageService {
     fun sendMessage(message: Message) {
         // TODO: Make call to socket service to send message.
     }
+
+    fun initialize() {
+        // TODO: Make call to socket service to listen to message receiving event and pass receiveMessage function.
+    }
+
+    fun receiveMessage(message: Message) {
+
+    }
 }
