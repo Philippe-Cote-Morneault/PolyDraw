85e354799bc76879f4acaa39fe2ac67137dda8db
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-19 20:30:04 -0500

    Changed message to hold a date instead of time.

--- Mobile/app/src/main/java/com/log3900/chat/Message.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message.kt
@@ -3,4 +3,4 @@ package com.log3900.chat
 import java.sql.Time
 import java.util.*
 
-data class Message(var message: String, var channelID:  UUID, var senderID: UUID, var senderName: String, var timestamp: Time)
+data class Message(var message: String, var channelID:  UUID, var senderID: UUID, var senderName: String, var timestamp: Date)
