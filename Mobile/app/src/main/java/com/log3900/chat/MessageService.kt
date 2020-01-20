e13ac6c94f1d96ecb54196a2b11317ff3bfd986e
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-19 19:08:48 -0500

    Added subscriber map for messageService.

--- Mobile/app/src/main/java/com/log3900/chat/MessageService.kt
+++ Mobile/app/src/main/java/com/log3900/chat/MessageService.kt
@@ -1,12 +1,16 @@
 package com.log3900.chat
 
+enum class MessageEvent {
+    MESSAGE_RECEIVED
+}
 class MessageService {
+    var subscribers: MutableMap<MessageEvent, (Message) -> Unit> = mutableMapOf()
 
     fun sendMessage(message: Message) {
         // TODO: Make call to socket service to send message.
     }
 
-    fun initialize() {
+    private fun initialize() {
         // TODO: Make call to socket service to listen to message receiving event and pass receiveMessage function.
     }
 
