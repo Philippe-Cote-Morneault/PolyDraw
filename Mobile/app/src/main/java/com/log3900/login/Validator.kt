5742c5893921aa52b58ad5692c7ac08c6ebaf233
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-01-22 14:48:07 -0500

    Added validator

---
+++ Mobile/app/src/main/java/com/log3900/login/Validator.kt
@@ -0,0 +1,15 @@
+package com.log3900.login
+
+object Validator {
+    private const val minLength = 4
+    private const val maxLength = 12
+
+    fun validateUsername(username: String) = username.isNotEmpty()
+            && username.isAlphanumeric()
+            && username.isValidLen()
+
+    fun validatePassword(password: String) = password.isNotEmpty() && password.isValidLen()
+
+    private fun String.isAlphanumeric(): Boolean = this.matches(Regex("[a-zA-Z0-9]"))
+    private fun String.isValidLen(): Boolean = this.length in minLength..maxLength
+}
