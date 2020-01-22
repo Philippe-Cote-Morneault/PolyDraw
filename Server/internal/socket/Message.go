6476e962c8411af3133ee154799bb7f91b9f89a5
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-22 12:03:02 -0500

    Refactored the code so there are no so many files

--- Server/internal/socket/Message.go
+++ Server/internal/socket/Message.go
@@ -1,6 +1,9 @@
 package socket
 
-import "time"
+import (
+	"encoding/binary"
+	"time"
+)
 
 // SerializableMessage Represents a serializable message sent over socket
 type SerializableMessage struct {
@@ -35,4 +38,33 @@ var MessageType = &messageType{
 	LeaveChannel:        24,
 	UserLeftChannel:     25,
 	CreateChannel:       26,
+}
+
+// Quick implementation of enum to represent different socket events.
+type socketEvent struct {
+	Connection    int
+	Disconnection int
+}
+
+// SocketEvent represents an event that can occur with sockets.
+var SocketEvent = &socketEvent{
+	Connection:    0,
+	Disconnection: 1,
+}
+
+// RawMessage represents a message that will not be serialized and be sent raw
+type RawMessage struct {
+	Type   byte
+	Length uint16
+	Bytes  []byte
+}
+
+// ToBytesSlice converts the raw message into the TLV format
+func (message *RawMessage) ToBytesSlice() []byte {
+	bytes := make([]byte, message.Length+3)
+	bytes[0] = message.Type
+	binary.BigEndian.PutUint16(bytes[1:], message.Length)
+	copy(bytes[3:], message.Bytes)
+
+	return bytes
 }
