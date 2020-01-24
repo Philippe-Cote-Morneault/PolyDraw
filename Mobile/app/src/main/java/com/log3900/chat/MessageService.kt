6ce1b7944c3486dbbaacc11e15833082e283e30e
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-23 23:25:10 -0500

    Changed MessageService to use ConcurrentHashMap and store Handlers instead of callbacks.

--- Mobile/app/src/main/java/com/log3900/chat/MessageService.kt
+++ Mobile/app/src/main/java/com/log3900/chat/MessageService.kt
@@ -1,21 +1,31 @@
 package com.log3900.chat
 
+import android.os.Handler
+import java.util.concurrent.ConcurrentHashMap
+
 enum class MessageEvent {
     MESSAGE_RECEIVED
 }
+
 class MessageService {
-    private var subscribers: MutableMap<MessageEvent, MutableList<(Message) -> Unit>> = mutableMapOf()
+    private var subscribers: ConcurrentHashMap<MessageEvent, ArrayList<Handler>> = ConcurrentHashMap()
+    // TODO: Add SocketService when it is implemented
+    //private lateinit var socketService: SocketService
+
+    constructor() {
+       initialize()
+    }
 
     fun sendMessage(message: Message) {
         // TODO: Make call to socket service to send message.
     }
 
-    fun subscribe(event: MessageEvent, callback: (Message) -> Unit) {
-        if (subscribers[event] == null) {
-            subscribers[event] = mutableListOf()
+    fun subscribe(event: MessageEvent, handler: Handler) {
+        if (!subscribers.containsKey(event)) {
+            subscribers[event] = ArrayList()
         }
 
-        subscribers[event]?.add(callback)
+        subscribers[event]?.add(handler)
     }
 
     fun receiveMessage(message: Message) {
