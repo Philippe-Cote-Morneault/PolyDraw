cb00936921c1230fae969350aedd8b855d2c6c74
AllanBeddouk <allan.beddouk@polymtl.ca>
2020-01-25 15:57:37 -0500

    Main temporaire pour tester les sockets

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.log3900
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.log3900.socketServices.SocketService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        var socketService = SocketService()
+        var socketTest = SocketTest(socketService)
+
+        StartButton.setOnClickListener{
+            socketTest.start()
+        }
+        StopButton.setOnClickListener{
+            socketTest.close()
+        }
+    }
+
+    private class SocketTest(var socketService: SocketService): Thread(){
+        override fun run() {
+            socketService.connectSocket()
+
+        }
+
+         fun close() {
+            socketService.deconnectSocket()
+
+        }
     }
 }
