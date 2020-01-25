944679f2e1dfe51c664d32fc39e0c91352a5ee6d
AllanBeddouk <allan.beddouk@polymtl.ca>
2020-01-25 15:58:37 -0500

    Creation du socketService pour co/deco les sockets

--- Mobile/app/src/main/java/com/log3900/socketServices/SocketService.kt
+++ Mobile/app/src/main/java/com/log3900/socketServices/SocketService.kt
@@ -4,6 +4,26 @@ import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.Socket
 
-class SocketService {
+//import java.io.BufferedOutputStream
+
+
+class SocketService{
+    private val socket:Socket = Socket()
+//    private lateinit var outputStream: ObjectOutputStream
+    private lateinit var readSocketThread: ReadSocketThread
+
+    fun connectSocket() {
+        println("Connexion...")
+        socket.connect(InetSocketAddress(InetAddress.getByName("192.168.0.99"), 4201))
+        println("Connexion etablie...")
+
+        readSocketThread = ReadSocketThread(socket)
+        readSocketThread.start()
+    }
+
+    fun deconnectSocket(){
+        readSocketThread.close()
+    }
+
 
 }
