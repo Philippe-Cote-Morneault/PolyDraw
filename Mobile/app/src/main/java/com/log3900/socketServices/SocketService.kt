32271c4443918e4983204de84bd954180b4d5bb3
AllanBeddouk <allan.beddouk@polymtl.ca>
2020-01-26 18:49:37 -0500

    ajout de la methode subscribe pour ajouter des subscribers

--- Mobile/app/src/main/java/com/log3900/socketServices/SocketService.kt
+++ Mobile/app/src/main/java/com/log3900/socketServices/SocketService.kt
@@ -1,23 +1,28 @@
 package com.log3900.socketServices
 
+import android.os.Handler
+import java.io.BufferedInputStream
 import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.Socket
+import java.io.BufferedOutputStream
 
-//import java.io.BufferedOutputStream
 
+enum class MessageEvent(var eventType: Int) {
+    MESSAGE_RECEIVED(20),
+}
 
 class SocketService{
-    private val socket:Socket = Socket()
-//    private lateinit var outputStream: ObjectOutputStream
+    private var socket:Socket = Socket()
+    private lateinit var outputStream: BufferedOutputStream
     private lateinit var readSocketThread: ReadSocketThread
 
     fun connectSocket() {
         println("Connexion...")
         socket.connect(InetSocketAddress(InetAddress.getByName("192.168.0.99"), 4201))
         println("Connexion etablie...")
-
-        readSocketThread = ReadSocketThread(socket)
+        outputStream = BufferedOutputStream(socket.getOutputStream())
+        readSocketThread = ReadSocketThread(BufferedInputStream(socket.getInputStream()))
         readSocketThread.start()
     }
 
@@ -25,5 +30,11 @@ class SocketService{
         readSocketThread.close()
     }
 
+    fun subscribe(event:MessageEvent, handler:Handler){
+        if (!readSocketThread.subscribers.containsKey(event)) {
+            readSocketThread.subscribers[event] = ArrayList()
+        }
 
+        readSocketThread.subscribers[event]?.add(handler)
+    }
 }
