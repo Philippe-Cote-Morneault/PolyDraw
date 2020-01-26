702472cd4eab12d963b365d82a2290f1cbff7f7d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-26 14:23:06 -0500

    Added method to subscribe to channels.

--- Mobile/app/src/main/java/com/log3900/chat/ChatFragment.kt
+++ Mobile/app/src/main/java/com/log3900/chat/ChatFragment.kt
@@ -35,9 +35,11 @@ class ChatFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView: View = inflater.inflate(R.layout.fragment_chat, container, false)
 
+        messageService = MessageService()
+
         setupUiElements(rootView)
 
-        messageService = MessageService()
+        subscribeToEvents()
 
         val thread = Thread{
             var i = 1
@@ -54,47 +56,8 @@ class ChatFragment : Fragment() {
 
         thread.start()
 
-
-
-        val handler = Handler(object: Handler.Callback {
-            override fun handleMessage(msg: android.os.Message): Boolean {
-                println("inside callback, tid = " + Thread.currentThread().id)
-                println(msg.toString())
-
-                messagesViewAdapter.messages.addLast(msg.obj as Message)
-                messagesViewAdapter.notifyItemInserted(messagesViewAdapter.messages.size - 1)
-                if (!messagesRecyclerView.canScrollVertically(1)) {
-                    messagesRecyclerView.smoothScrollToPosition(messagesViewAdapter.messages.size - 1)
-                }
-                return true
-            }
-        })
-
-        messageService.subscribe(MessageEvent.MESSAGE_RECEIVED, handler)
-
-        val messagesTest: LinkedList<Message> = LinkedList()
-        messagesTest.add(Message("user send this 1", UUID.randomUUID(), UUID.randomUUID(), "admin", Date()))
-        for (i in 0..10) {
-            messagesTest.add(Message("test" + i, UUID.randomUUID(), UUID.randomUUID(), "sender" + i, Date()))
-        }
-
-        messagesTest.add(Message("user send this 2", UUID.randomUUID(), UUID.randomUUID(), "admin", Date()))
-
         return rootView
     }
-
-    inner class SendMessageButtonListener : View.OnClickListener {
-        override fun onClick(v: View) {
-            val messageInput: TextInputEditText = v.rootView.findViewById(R.id.fragment_chat_new_message_input)
-            val messageText = messageInput.text.toString()
-            messageInput.text?.clear()
-            messageService.sendMessage(messageText)
-
-            KeyboardHelper.hideKeyboard(activity as Activity)
-
-        }
-    }
-
 
     /**
      * Used to assign all variables holding UI elements for this fragment and setup listeners.
@@ -107,7 +70,7 @@ class ChatFragment : Fragment() {
         setupMessagesRecyclerView(rootView)
 
         sendMessageButton = rootView.findViewById(R.id.fragment_chat_send_message_button)
-        sendMessageButton.setOnClickListener(SendMessageButtonListener())
+        sendMessageButton.setOnClickListener { v -> onSendMessageButtonClick(v)}
 
         setupToolbar(rootView)
 
@@ -132,6 +95,15 @@ class ChatFragment : Fragment() {
         }
     }
 
+    private fun subscribeToEvents() {
+        val handler = Handler{ msg ->
+            onNewMessageReceived(msg)
+            true
+        }
+
+        messageService.subscribe(MessageEvent.MESSAGE_RECEIVED, handler)
+    }
+
     /**
      * Handles click on the top toolbar navigation icon. Closes and opens the navigation drawer, which contains the channel list.
      *
@@ -141,6 +113,29 @@ class ChatFragment : Fragment() {
             drawer.closeDrawer(Gravity.LEFT)
         } else {
             drawer.openDrawer(Gravity.LEFT)
+        }
+    }
+
+    private fun onSendMessageButtonClick(v: View) {
+        val messageInput: TextInputEditText = v.rootView.findViewById(R.id.fragment_chat_new_message_input)
+        val messageText = messageInput.text.toString()
+        messageInput.text?.clear()
+        messageService.sendMessage(messageText)
+
+        KeyboardHelper.hideKeyboard(activity as Activity)
+
+    }
+
+    /**
+     * Handles a new chat message received. Informs the recyclerView of its presence.
+     *
+     * @param message the chat message received
+     */
+    private fun onNewMessageReceived(message: android.os.Message) {
+        messagesViewAdapter.messages.addLast(message.obj as Message)
+        messagesViewAdapter.notifyItemInserted(messagesViewAdapter.messages.size - 1)
+        if (!messagesRecyclerView.canScrollVertically(1)) {
+            messagesRecyclerView.smoothScrollToPosition(messagesViewAdapter.messages.size - 1)
         }
     }
 }
