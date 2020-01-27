0981a6fc1277107ae36c87b06a9f717838854962
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-26 22:18:00 -0500

    Remove the disconnect socket event since it's the same as close

--- Server/internal/socket/broadcast.go
+++ Server/internal/socket/broadcast.go
@@ -5,10 +5,6 @@ import "gitlab.com/jigsawcorp/log3900/pkg/cbroadcast"
 //BSocketReady broadcasted when the socket is ready
 const BSocketReady = "socket:ready"
 
-//BSocketDisconnect broadcasted when the socket is disconnected for a client with the disconnect message
-//Sends the client id a uuid
-const BSocketDisconnect = "socket:disconnect"
-
 //BSocketConnected broadcasted when the socket is connected for a client
 //Sends the client id a uuid
 const BSocketConnected = "socket:connect"
@@ -31,7 +27,6 @@ const BSize = 20
 //RegisterBroadcast Register all the broadcast for the socket
 func RegisterBroadcast() {
 	cbroadcast.Register(BSocketReady, BSize)
-	cbroadcast.Register(BSocketDisconnect, BSize)
 	cbroadcast.Register(BSocketConnected, BSize)
 	cbroadcast.Register(BSocketCloseClient, BSize)
 	cbroadcast.Register(BSocketClose, BSize)
