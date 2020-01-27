541d9bb624b921dd8dbde3d22f9f4e70da726471
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-26 22:16:07 -0500

    Remove the log service and moved it to the logger service

--- Server/cmd/server/main.go
+++ Server/cmd/server/main.go
@@ -4,8 +4,11 @@ import (
 	"log"
 
 	"gitlab.com/jigsawcorp/log3900/internal/rest"
+	service "gitlab.com/jigsawcorp/log3900/internal/services"
+	"gitlab.com/jigsawcorp/log3900/internal/services/logger"
+	"gitlab.com/jigsawcorp/log3900/internal/services/messenger"
+	"gitlab.com/jigsawcorp/log3900/internal/services/router"
 	"gitlab.com/jigsawcorp/log3900/internal/socket"
-	"gitlab.com/jigsawcorp/log3900/pkg/cbroadcast"
 	"gitlab.com/jigsawcorp/log3900/pkg/graceful"
 )
 
@@ -16,7 +19,11 @@ func main() {
 
 	graceful.Register(restServer.Shutdown, "REST server")
 	graceful.Register(socketServer.Shutdown, "Socket server")
+	graceful.Register(service.ShutdownAll, "Services") //Shutdown all services
+
 	graceful.ListenSIG()
+
+	registerServices()
 
 	log.Printf("Server is starting jobs!")
 
@@ -33,34 +40,14 @@ func main() {
 		handleSocket <- true
 	}()
 
-	//Message viewer
-	go func() {
-		ready, _, _ := cbroadcast.Subscribe(socket.BSocketReady)
-		receiver, _, _ := cbroadcast.Subscribe(socket.BSocketReceive)
-		connected, _, _ := cbroadcast.Subscribe(socket.BSocketConnected)
-		close, _, _ := cbroadcast.Subscribe(socket.BSocketCloseClient)
-
-		for {
-			select {
-			case <-ready:
-				log.Println("Socket is ready")
-			case data := <-receiver:
-				message, ok := data.(socket.RawMessage)
-
-				if ok {
-					log.Printf("Socket data received: %#x | %d | %#x -> %s", message.Type, message.Length, message.Bytes, message.Bytes)
-				}
-
-				//fmt.Printf("data: %s | %#x\n", data, data)
-			case id := <-connected:
-				log.Printf("connected id: %s", id)
-			case id := <-close:
-				log.Printf("disconnect id: %s", id)
-			}
-		}
-
-	}()
+	service.StartAll()
 
 	<-handleRest
 	<-handleSocket
+}
+
+func registerServices() {
+	service.Add(&logger.Logger{})
+	service.Add(&router.Router{})
+	service.Add(&messenger.Messenger{})
 }
---
+++ Server/internal/services/logger/logger.go
@@ -0,0 +1,73 @@
+package logger
+
+import (
+	"log"
+
+	"gitlab.com/jigsawcorp/log3900/internal/socket"
+	"gitlab.com/jigsawcorp/log3900/pkg/cbroadcast"
+)
+
+//Logger service used to debug the message received by the server
+type Logger struct {
+	ready     cbroadcast.Channel
+	receiver  cbroadcast.Channel
+	connected cbroadcast.Channel
+	close     cbroadcast.Channel
+
+	shutdown chan bool
+}
+
+//Init the messenger service
+func (l *Logger) Init() {
+	l.shutdown = make(chan bool)
+	l.subscribe()
+}
+
+//Start the messenger service
+func (l *Logger) Start() {
+	log.Println("[Logger] -> Starting service")
+	go l.listen()
+}
+
+//Shutdown the messenger service
+func (l *Logger) Shutdown() {
+	log.Println("[Logger] -> Closing service")
+	close(l.shutdown)
+}
+
+//Register register any broadcast not used
+func (l *Logger) Register() {
+
+}
+
+func (l *Logger) listen() {
+	//Message viewer
+	for {
+		select {
+		case <-l.ready:
+			log.Println("[Logger] -> Socket is ready")
+		case data := <-l.receiver:
+			message, ok := data.(socket.RawMessageReceived)
+
+			if ok {
+				log.Printf("[Logger] -> Socket data received: %d | %d | %#x", message.Payload.MessageType, message.Payload.Length, message.Payload.Bytes)
+			}
+
+			//fmt.Printf("data: %s | %#x\n", data, data)
+		case id := <-l.connected:
+			log.Printf("[Logger] -> connected id: %s", id)
+		case id := <-l.close:
+			log.Printf("[Logger] -> disconnect id: %s", id)
+		case <-l.shutdown:
+			return
+		}
+	}
+
+}
+
+func (l *Logger) subscribe() {
+	l.ready, _, _ = cbroadcast.Subscribe(socket.BSocketReady)
+	l.receiver, _, _ = cbroadcast.Subscribe(socket.BSocketReceive)
+	l.connected, _, _ = cbroadcast.Subscribe(socket.BSocketConnected)
+	l.close, _, _ = cbroadcast.Subscribe(socket.BSocketCloseClient)
+}
