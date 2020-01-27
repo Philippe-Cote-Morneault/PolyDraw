8802cab5b5c37dfa3c16a0636fc9844d5ac972f8
AllanBeddouk <allan.beddouk@polymtl.ca>
2020-01-26 20:19:06 -0500

    Division de message entre deux messages received and sent

--- Mobile/app/src/main/java/com/log3900/socketServices/Message.kt
+++ Mobile/app/src/main/java/com/log3900/socketServices/Message.kt
@@ -2,4 +2,10 @@ package com.log3900.socketServices
 
 import java.util.*
 
-data class Message(var message: String, var channelID:  UUID, var senderID: UUID, var senderName: String, var timestamp: Date)
+data class MessageReceived(var message: String, var channelID:  UUID, var senderID: UUID, var senderName: String, var timestamp: Date)
+data class MessageSent(var message: String, var channelID:  UUID)
+
+enum class MessageEvent(var eventType: Int) {
+    MESSAGE_RECEIVED(21),
+    MESSAGE_SENT(20)
+}
