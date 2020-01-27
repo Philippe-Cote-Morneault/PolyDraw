3878c3e7e56b6a116a70860a370afc0d90b13f9b
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-26 20:32:25 -0500

    Added onCreateView() for ChannelFragment.

--- Mobile/app/src/main/java/com/log3900/chat/ChannelFragment.kt
+++ Mobile/app/src/main/java/com/log3900/chat/ChannelFragment.kt
@@ -1,12 +1,17 @@
 package com.log3900.chat
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.log3900.R
 
 class ChannelFragment : Fragment() {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val rootView: View = inflater.inflate(R.layout.fragment_channel, container, false)
+
+        return rootView
     }
 
 }
