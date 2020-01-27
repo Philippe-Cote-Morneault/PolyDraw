3e6c8a7835f7b77f9d86bbb8bb61f7b3e10ab576
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-26 20:09:55 -0500

    Added ChannelFragment.

--- Mobile/app/src/main/java/com/log3900/chat/ChannelFragment.kt
+++ Mobile/app/src/main/java/com/log3900/chat/ChannelFragment.kt
@@ -1,2 +1,12 @@
 package com.log3900.chat
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class ChannelFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
