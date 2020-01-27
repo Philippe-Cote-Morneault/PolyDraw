6eb9faf1c8814af517613c378d606d80ad818de5
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-26 22:17:26 -0500

    Created a router for the socket messages for the services

---
+++ Server/internal/services/router/router.go
@@ -0,0 +1,87 @@
+package router
+
+import (
+	"fmt"
+	"gitlab.com/jigsawcorp/log3900/internal/socket"
+	"gitlab.com/jigsawcorp/log3900/pkg/cbroadcast"
+	"log"
+)
+
+//Router the router service
+type Router struct {
+	receiveChan       cbroadcast.Channel
+	closingSocketChan cbroadcast.Channel
+	closingChan       cbroadcast.Channel
+	shutdown          chan bool
+	routes            map[byte]string
+}
+
+//Init the router must be called before start
+func (r *Router) Init() {
+	r.shutdown = make(chan bool)
+	r.routes = make(map[byte]string)
+	r.subscribe()
+
+	r.routing()
+}
+
+// Register any broadcast for the router. not used for this service
+func (r *Router) Register() {
+
+}
+
+//Start the router service
+func (r *Router) Start() {
+	log.Println("[SRouter] -> Starting service")
+	go r.listen()
+}
+
+//Shutdown the router service
+func (r *Router) Shutdown() {
+	log.Println("[SRouter] -> Closing service")
+	close(r.shutdown)
+}
+
+func (r *Router) listen() {
+	for {
+		select {
+		case data := <-r.receiveChan:
+			message, ok := data.(socket.RawMessageReceived)
+
+			if ok {
+				//Route the message to the correct service
+				r.route(message)
+			}
+		case <-r.shutdown:
+			return
+		}
+	}
+
+}
+
+//route the message to the correct service
+func (r *Router) route(message socket.RawMessageReceived) {
+
+	//Check for a handle and broadcast the message
+	if broadcast, ok := r.routes[message.Payload.MessageType]; ok {
+		cbroadcast.Broadcast(broadcast, message)
+	} else {
+		log.Printf("[SRouter] -> No route for %d", message.Payload.MessageType)
+	}
+}
+
+//handle a route that the router needs to handle
+func (r *Router) handle(messageType int, broadcastName string) error {
+	if messageType > 255 || messageType < 0 {
+		return fmt.Errorf("Can not have a message type bellow 0 or over 255")
+	}
+	r.routes[byte(messageType)] = broadcastName
+
+	return nil
+}
+
+func (r *Router) subscribe() {
+	r.receiveChan, _, _ = cbroadcast.Subscribe(socket.BSocketReceive)
+	r.closingSocketChan, _, _ = cbroadcast.Subscribe(socket.BSocketClose)
+	r.closingChan, _, _ = cbroadcast.Subscribe(socket.BSocketCloseClient)
+}
--- Server/internal/services/router/routes.go
+++ Server/internal/services/router/routes.go
@@ -1,29 +1,11 @@
 package router
 
-// Quick implementation of enum to represent different message types
-type messageType struct {
-	ServerConnection    int
-	ServerDisconnection int
-	UserDisconnected    int
-	MessageSent         int
-	MessageReceived     int
-	JoinChannel         int
-	UserJoinedChannel   int
-	LeaveChannel        int
-	UserLeftChannel     int
-	CreateChannel       int
-}
+import (
+	"gitlab.com/jigsawcorp/log3900/internal/services/messenger"
+	"gitlab.com/jigsawcorp/log3900/internal/socket"
+)
 
-// MessageType represents the available message types to send to clients.
-var MessageType = &messageType{
-	ServerConnection:    0,
-	ServerDisconnection: 1,
-	UserDisconnected:    2,
-	MessageSent:         20,
-	MessageReceived:     21,
-	JoinChannel:         22,
-	UserJoinedChannel:   23,
-	LeaveChannel:        24,
-	UserLeftChannel:     25,
-	CreateChannel:       26,
+func (r *Router) routing() {
+	r.handle(socket.MessageType.MessageSent, messenger.BMessageSent)
+	//TODO include the authentification middleware
 }
