58ea0e231e15218330f5dad6a0809ffcd67bfa9f
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-26 22:18:22 -0500

    Moved message to avoid circular dependencies

--- Server/internal/socket/message.go
+++ Server/internal/socket/message.go
@@ -2,42 +2,98 @@ package socket
 
 import (
 	"encoding/binary"
+	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/vmihailenco/msgpack/v4"
 )
+
+// Quick implementation of enum to represent different message types
+type messageType struct {
+	ServerConnection    int
+	ServerDisconnection int
+	UserDisconnected    int
+	MessageSent         int
+	MessageReceived     int
+	JoinChannel         int
+	UserJoinedChannel   int
+	LeaveChannel        int
+	UserLeftChannel     int
+	CreateChannel       int
+}
+
+// MessageType represents the available message types to send to clients.
+var MessageType = &messageType{
+	ServerConnection:    0,
+	ServerDisconnection: 1,
+	UserDisconnected:    2,
+	MessageSent:         20,
+	MessageReceived:     21,
+	JoinChannel:         22,
+	UserJoinedChannel:   23,
+	LeaveChannel:        24,
+	UserLeftChannel:     25,
+	CreateChannel:       26,
+}
 
 // SerializableMessage Represents a serializable message sent over socket
 type SerializableMessage struct {
-	messageType int
-	data        interface{}
+	MessageType int
+	Data        interface{}
 }
 
 // RawMessage represents a message that will not be serialized and be sent raw
 type RawMessage struct {
-	messageType byte
-	length      uint16
-	bytes       []byte
+	MessageType byte
+	Length      uint16
+	Bytes       []byte
 }
 
 //RawMessageReceived represents a message that was received. It is associated with a client id
 type RawMessageReceived struct {
-	message  RawMessage
-	socketid uuid.UUID
+	Payload  RawMessage
+	SocketID uuid.UUID
 }
 
 // ToBytesSlice converts the raw message into the TLV format
 func (message *RawMessage) ToBytesSlice() []byte {
-	bytes := make([]byte, message.length+3)
-	bytes[0] = message.messageType
-	binary.BigEndian.PutUint16(bytes[1:], message.length)
-	copy(bytes[3:], message.bytes)
+	bytes := make([]byte, message.Length+3)
+	bytes[0] = message.MessageType
+	binary.BigEndian.PutUint16(bytes[1:], message.Length)
+	copy(bytes[3:], message.Bytes)
 
 	return bytes
 }
 
 // ParseMessage create the message object
 func (message *RawMessage) ParseMessage(typeMessage byte, length uint16, bytes []byte) {
-	message.messageType = typeMessage
-	message.length = length
-	message.bytes = bytes[3:]
+	message.MessageType = typeMessage
+	message.Length = length
+	message.Bytes = bytes[3:]
+}
+
+// ParseMessagePack parse message pack into a RawMessage
+func (message *RawMessage) ParseMessagePack(typeMessage byte, v interface{}) error {
+	bytes, err := msgpack.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	if len(bytes) > 65535 {
+		return fmt.Errorf("cannot have bytes larger than 65535")
+	}
+	message.Length = uint16(len(bytes))
+	message.MessageType = typeMessage
+	message.Bytes = bytes
+
+	return nil
+}
+
+// DecodeMessagePack parse the message into MessagePack
+func (message *RawMessage) DecodeMessagePack(v interface{}) error {
+	err := msgpack.Unmarshal(message.Bytes, v)
+	if err != nil {
+		return err
+	}
+	return nil
 }
