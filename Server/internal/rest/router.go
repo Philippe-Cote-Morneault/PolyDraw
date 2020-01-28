e34e36835bbf62c946ea802fd41a6c52ed04d2a5
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-27 20:05:15 -0500

    Added auth routes

--- Server/internal/rest/router.go
+++ Server/internal/rest/router.go
@@ -11,6 +11,8 @@ import (
 func (a *Server) setRouters() {
 	a.Get("/hello", a.handleRequest(api.GetHello))
 	a.Get("/hello/{title}", a.handleRequest(api.GetHello))
+	a.Post("/auth", a.handleRequest(api.PostAuth))
+	a.Post("/auth/token", a.handleRequest(api.PostAuthToken))
 }
 
 func defaultRoute(w http.ResponseWriter, r *http.Request) {
