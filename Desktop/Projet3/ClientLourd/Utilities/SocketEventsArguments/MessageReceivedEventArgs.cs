54d1e5550913bead6b7e6add348d516702f4286e
cedric <cedric.tessier@polymtl.ca>
2020-01-28 10:43:59 -0500

    Added a new socket event args

---
+++ Desktop/Projet3/ClientLourd/Utilities/SocketEventsArguments/MessageReceivedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClientLourd.Utilities.SocketEventsArguments
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(dynamic data)
+        {
+            ChannelId = data["ChannelID"];
+            Date = DateTime.Parse(data["timestamp"]);
+            ChannelId = data["UserID"];
+            UserName = data["UserName"];
+            UserId = data["UserId"];
+        }
+        
+        public int ChannelId { get; private set; }
+        public DateTime Date { get; private set; }
+        public string UserName { get; private set; }
+        public int UserId { get; private set; }
+    }
+}
