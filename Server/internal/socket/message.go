5e6a221095e1dda8a70d1ae08ea83ac4623c5513
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-28 00:58:52 -0500

    Added the server connection response

--- Server/internal/socket/message.go
+++ Server/internal/socket/message.go
@@ -10,30 +10,32 @@ import (
 
 // Quick implementation of enum to represent different message types
 type messageType struct {
-	ServerConnection    int
-	ServerDisconnection int
-	UserDisconnected    int
-	MessageSent         int
-	MessageReceived     int
-	JoinChannel         int
-	UserJoinedChannel   int
-	LeaveChannel        int
-	UserLeftChannel     int
-	CreateChannel       int
+	ServerConnection         int
+	ServerConnectionResponse int
+	ServerDisconnection      int
+	UserDisconnected         int
+	MessageSent              int
+	MessageReceived          int
+	JoinChannel              int
+	UserJoinedChannel        int
+	LeaveChannel             int
+	UserLeftChannel          int
+	CreateChannel            int
 }
 
 // MessageType represents the available message types to send to clients.
 var MessageType = &messageType{
-	ServerConnection:    0,
-	ServerDisconnection: 1,
-	UserDisconnected:    2,
-	MessageSent:         20,
-	MessageReceived:     21,
-	JoinChannel:         22,
-	UserJoinedChannel:   23,
-	LeaveChannel:        24,
-	UserLeftChannel:     25,
-	CreateChannel:       26,
+	ServerConnection:         0,
+	ServerConnectionResponse: 1,
+	UserDisconnected:         2,
+	ServerDisconnection:      3,
+	MessageSent:              20,
+	MessageReceived:          21,
+	JoinChannel:              22,
+	UserJoinedChannel:        23,
+	LeaveChannel:             24,
+	UserLeftChannel:          25,
+	CreateChannel:            26,
 }
 
 // SerializableMessage Represents a serializable message sent over socket
