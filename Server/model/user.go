d5facec1d11b426499102e0dace06c8112506c70
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-27 20:30:38 -0500

    Added firstname & lastname

--- Server/model/user.go
+++ Server/model/user.go
@@ -3,6 +3,8 @@ package model
 //User represented in the database
 type User struct {
 	Base
+	FirstName      string
+	LastName       string
 	Username       string
 	Email          string
 	HashedPassword string
