595e9413b8d658b6bc0a411b56605edd06576269
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-01-28 17:54:04 -0500

    Removed button calls.

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -10,29 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-        var socketService = SocketService()
-        var socketTest = SocketTest(socketService)
-
-        StartButton.setOnClickListener{
-            socketTest.start()
-        }
-        StopButton.setOnClickListener{
-            socketTest.close()
-        }
-    }
-
-    private class SocketTest(var socketService: SocketService): Thread(){
-        override fun run() {
-            socketService.connectSocket()
-
-        }
-
-         fun close() {
-            socketService.deconnectSocket()
-
-        }
     }
 }
