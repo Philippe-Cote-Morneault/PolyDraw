4f1427287b4b5cc8efef11f9eaff9600e63e5330
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-01-28 02:15:26 -0500

    Added rest api for auth service

--- Server/internal/api/authentification.go
+++ Server/internal/api/authentification.go
@@ -51,13 +51,26 @@ func PostAuth(w http.ResponseWriter, r *http.Request) {
 		model.DB().Create(&user)
 	}
 
-	//Generate session
-	sessionToken, _ := secureb.GenerateToken()
-	for !auth.IsTokenAvailable(sessionToken) {
-		sessionToken, _ = secureb.GenerateToken()
+	//Check if there is already a session
+	if auth.HasUserSession(user.ID) {
+		//There is already a session we abort the creation of a new session
+		rbody.JSONError(w, 401, "The user already has an other session tied to this account. Please disconnect the other session before connecting.")
+		return
 	}
-	auth.Register(sessionToken, user.ID)
-	rbody.JSON(w, http.StatusOK, authResponse{Bearer: user.Bearer, SessionToken: sessionToken})
+
+	if ok, sessionToken := auth.HasUserToken(user.ID); ok {
+		//The token is already registered
+		rbody.JSON(w, http.StatusOK, authResponse{Bearer: user.Bearer, SessionToken: sessionToken})
+	} else {
+		//Generate session token
+		sessionToken, _ := secureb.GenerateToken()
+		for !auth.IsTokenAvailable(sessionToken) {
+			sessionToken, _ = secureb.GenerateToken()
+		}
+		auth.Register(sessionToken, user.ID)
+		rbody.JSON(w, http.StatusOK, authResponse{Bearer: user.Bearer, SessionToken: sessionToken})
+	}
+
 }
 
 //PostAuthToken authenticate using the bearer token
