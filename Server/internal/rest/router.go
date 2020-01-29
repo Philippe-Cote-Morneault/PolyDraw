a406f7368496bc7c1000a83c951b6732c9d6f27a
AllanBeddouk <allan.beddouk@polymtl.ca>
2020-01-29 11:53:05 -0500

    Ajout des routes pour avoir tous les users et pour recuperr l'username

--- Server/internal/rest/router.go
+++ Server/internal/rest/router.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 
 	"gitlab.com/jigsawcorp/log3900/internal/api"
+	"gitlab.com/jigsawcorp/log3900/model"
 	"gitlab.com/jigsawcorp/log3900/pkg/rbody"
 )
 
 // setRouters sets the all required routers
 func (a *Server) setRouters() {
 	a.Get("/hello", a.handleRequest(api.GetHello))
-	a.Get("/hello/{title}", a.handleRequest(api.GetHello))
+	a.Post("/auth", a.handleRequest(api.LoginUser))
+	a.Get("/allUsers", a.handleRequest(model.AllUsers))
 }
 
 func defaultRoute(w http.ResponseWriter, r *http.Request) {
