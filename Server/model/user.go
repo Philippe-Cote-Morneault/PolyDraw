62d95794d66952de4b04e82a07cf9f8340a2cd54
AllanBeddouk <allan.beddouk@polymtl.ca>
2020-01-29 11:53:51 -0500

    Ajout des fonctions allUsers, findUserByName and addUser pour la bd

--- Server/model/user.go
+++ Server/model/user.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 //User represented in the database
 type User struct {
 	Base
@@ -7,4 +12,24 @@ type User struct {
 	Email          string
 	HashedPassword string
 	Bearer         string
+}
+
+// AllUsers Function Test to get All Users #Allan
+func AllUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	DB().Find(&users)
+	json.NewEncoder(w).Encode(users)
+
+}
+
+// FindUserByName Function to find a User by Name
+func FindUserByName(username string) bool {
+	var user User
+	DB().Where("username = ?", username).Find(&user)
+	return user.Username == username
+}
+
+// AddUser add user in DB
+func AddUser(user *User) {
+	DB().Create(&user)
 }
