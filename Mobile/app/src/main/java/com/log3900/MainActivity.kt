83c7f726f3ec9bb781a1e10a0dde9f3913bc14c1
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-01-29 18:02:58 -0500

    Revert "Login activity start from main application"
    This reverts commit ded4fd4df93def18e81c1671e63277bfcf91b561.

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -1,39 +1,20 @@
 package com.log3900
 
-import android.app.Application
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.log3900.login.LoginActivity
 import androidx.appcompat.app.AppCompatActivity
-import com.log3900.chat.ChatFragment
 import com.log3900.socketServices.SocketService
 import kotlinx.android.synthetic.main.activity_main.*
 
-class PolyDraw : Application() {
+class MainActivity : AppCompatActivity() {
 
-    init {
-        instance = this
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    companion object {
-        private var instance: PolyDraw? = null
-
-        fun applicationContext() : Context {
-            return instance!!.applicationContext
-        }
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        // initialize for any
-
-        // Use ApplicationContext.
-        // example: SharedPreferences etc...
-        // TODO: Check bearer token
+        /* TODO: token validation */
 
         val intent = Intent(this, LoginActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
 }
