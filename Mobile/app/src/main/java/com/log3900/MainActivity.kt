ded4fd4df93def18e81c1671e63277bfcf91b561
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-01-29 17:25:23 -0500

    Login activity start from main application

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.log3900
 
+import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.log3900.login.LoginActivity
 import androidx.appcompat.app.AppCompatActivity
+import com.log3900.chat.ChatFragment
 import com.log3900.socketServices.SocketService
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class PolyDraw : Application() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    init {
+        instance = this
+    }
 
-        /* TODO: token validation */
+    companion object {
+        private var instance: PolyDraw? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // initialize for any
+
+        // Use ApplicationContext.
+        // example: SharedPreferences etc...
+        // TODO: Check bearer token
 
         val intent = Intent(this, LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
 }
