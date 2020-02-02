450739a79a276c5af74dfb2cbaf4bc93240a6dd1
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 14:48:44 -0500

    Launcher activity creates SocketService and MainActivity.

--- Mobile/app/src/main/java/com/log3900/LauncherActivity.kt
+++ Mobile/app/src/main/java/com/log3900/LauncherActivity.kt
@@ -6,12 +6,19 @@ import android.content.Intent
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import android.os.Handler
+import com.log3900.socket.SocketHandler
+import com.log3900.socket.SocketService
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.net.Socket
+import java.util.concurrent.CountDownLatch
 
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startService(Intent(this, SocketService::class.java))
         val intent = Intent(this, MainActivity::class.java)// New activity
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
