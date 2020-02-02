e397cd8b65b296c56a003d5d158d67ad7af7eaf0
cedric <cedric.tessier@polymtl.ca>
2020-02-01 20:11:01 -0500

    Added a IsWaiting property in loginView to disabled to button

--- Desktop/Projet3/ClientLourd/Views/Login.xaml.cs
+++ Desktop/Projet3/ClientLourd/Views/Login.xaml.cs
@@ -38,5 +38,17 @@ namespace ClientLourd.Views
         {
             NameTextBox.Focus();
         }
+        
+        public static readonly DependencyProperty IsWaitingProperty = DependencyProperty.Register("IsWaiting", typeof(Boolean), typeof(Login), new PropertyMetadata(false));
+
+
+        public bool IsWaiting
+        {
+            get { return (bool)GetValue(IsWaitingProperty); }
+            set { SetValue(IsWaitingProperty, value); }
+        }
+
+        
+        
     }
 }
