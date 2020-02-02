3dddb79add572b88fca6ff60da57f85074a6ab83
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 14:52:06 -0500

    Removed useless imports.

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -2,8 +2,6 @@ package com.log3900
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.log3900.socketServices.SocketService
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
