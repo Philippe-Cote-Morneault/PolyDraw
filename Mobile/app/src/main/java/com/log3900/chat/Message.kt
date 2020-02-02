2dd46fdfb6081a2b3b439a5f2027e845c3e25201
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 18:12:59 -0500

    Added annotations to transform Messages to PascalCase when encoded.

--- Mobile/app/src/main/java/com/log3900/chat/Message.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message.kt
@@ -1,8 +1,10 @@
 package com.log3900.chat
 
+import com.squareup.moshi.Json
 import java.sql.Time
 import java.util.*
 
-data class ReceivedMessage(var message: String, var channelID:  String, var senderID: String, var senderName: String, var timestamp: Date)
+data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  String, @Json(name = "SenderID") var senderID: String,
+                           @Json(name = "SenderName") var senderName: String, @Json(name = "TimeStamp") var timestamp: Date)
 
-data class SentMessage(var message: String, var channelID: String)
+data class SentMessage(@Json(name = "Message")var message: String, @Json(name = "ChannelID") var channelID: String)
