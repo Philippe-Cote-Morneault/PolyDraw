218966795c5f7a42d327b9fc7e570a773d4555ea
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 20:05:11 -0500

    Changed back String to UUID in Messages.

--- Mobile/app/src/main/java/com/log3900/chat/Message.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import java.sql.Time
 import java.util.*
 
-data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  String, @Json(name = "SenderID") var senderID: String,
+data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  UUID, @Json(name = "SenderID") var senderID: UUID,
                            @Json(name = "SenderName") var senderName: String, @Json(name = "TimeStamp") var timestamp: Date)
 
-data class SentMessage(@Json(name = "Message")var message: String, @Json(name = "ChannelID") var channelID: String)
+data class SentMessage(@Json(name = "Message")var message: String, @Json(name = "ChannelID") var channelID: UUID)
