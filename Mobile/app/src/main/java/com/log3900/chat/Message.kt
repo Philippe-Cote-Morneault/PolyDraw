64742feb87f2edb8c274bf297e9938b4a431bbc7
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 22:27:32 -0500

    Removed useless import.

--- Mobile/app/src/main/java/com/log3900/chat/Message.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message.kt
@@ -1,7 +1,6 @@
 package com.log3900.chat
 
 import com.squareup.moshi.Json
-import java.sql.Time
 import java.util.*
 
 data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  UUID, @Json(name = "SenderID") var senderID: UUID,
