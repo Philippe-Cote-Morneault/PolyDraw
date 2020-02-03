3fa36535eb8c1fb075c1813e768afe3158589e8f
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 22:15:09 -0500

    LauncherActivity starts LoginActivity.

--- Mobile/app/src/main/java/com/log3900/LauncherActivity.kt
+++ Mobile/app/src/main/java/com/log3900/LauncherActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.os.Handler
+import com.log3900.login.LoginActivity
 import com.log3900.socket.SocketHandler
 import com.log3900.socket.SocketService
 import java.io.DataInputStream
@@ -19,7 +20,7 @@ class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startService(Intent(this, SocketService::class.java))
-        val intent = Intent(this, MainActivity::class.java)// New activity
+        val intent = Intent(this, LoginActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
         finish()
