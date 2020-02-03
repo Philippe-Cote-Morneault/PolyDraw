6bfc2cc95e67874c2950cbdba79ce0fc2649d390
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-02 22:16:16 -0500

    Fixed Timestamp annotation.

--- Mobile/app/src/main/java/com/log3900/chat/Message.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message.kt
@@ -5,6 +5,6 @@ import java.sql.Time
 import java.util.*
 
 data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  UUID, @Json(name = "SenderID") var senderID: UUID,
-                           @Json(name = "SenderName") var senderName: String, @Json(name = "TimeStamp") var timestamp: Date)
+                           @Json(name = "SenderName") var senderName: String, @Json(name = "Timestamp") var timestamp: Date)
 
 data class SentMessage(@Json(name = "Message")var message: String, @Json(name = "ChannelID") var channelID: UUID)
