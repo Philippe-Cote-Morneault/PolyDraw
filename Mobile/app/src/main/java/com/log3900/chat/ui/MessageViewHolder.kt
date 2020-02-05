3af8a97d1d5d027ca7652fb89bd58226b71b6fe0
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-05 16:12:11 -0500

    Fixed layout for chat messages.

--- Mobile/app/src/main/java/com/log3900/chat/ui/MessageViewHolder.kt
+++ Mobile/app/src/main/java/com/log3900/chat/ui/MessageViewHolder.kt
@@ -1,10 +1,16 @@
 package com.log3900.chat.ui
 
+import android.graphics.Color
+import android.opengl.Visibility
+import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import com.log3900.R
 import com.log3900.chat.ReceivedMessage
 import com.log3900.utils.format.DateFormatter
@@ -15,6 +21,7 @@ class MessageViewHolder : RecyclerView.ViewHolder {
     private var usernameTextView: TextView
     private var dateTextView: TextView
     private var username: String
+    private var messageBoxCardView: CardView
     private lateinit var message: ReceivedMessage
 
     constructor(itemView: View, username: String) : super(itemView) {
@@ -23,6 +30,7 @@ class MessageViewHolder : RecyclerView.ViewHolder {
         usernameTextView = itemView.findViewById(R.id.list_item_message_header)
         dateTextView = itemView.findViewById(R.id.list_item_message_date)
         messageTextView.maxLines = Integer.MAX_VALUE
+        messageBoxCardView = itemView.findViewById(R.id.list_item_message_text_card_view)
         this.username = username
     }
 
@@ -38,10 +46,21 @@ class MessageViewHolder : RecyclerView.ViewHolder {
         if (message.senderName == username) {
             constraintSet.clear(R.id.list_item_message_inner_layout, ConstraintSet.START)
             constraintSet.connect(R.id.list_item_message_inner_layout, ConstraintSet.END, R.id.list_item_message_outer_layout, ConstraintSet.END, 15)
+            messageBoxCardView.setBackgroundColor(Color.parseColor("#3F51B5"))
+            view.findViewById<LinearLayout>(R.id.list_item_message_inner_layout).gravity = Gravity.END
+            usernameTextView.visibility = View.GONE
+            usernameTextView.setTextColor(Color.parseColor("#FFFFFF"))
+            usernameTextView.gravity = Gravity.END
+            dateTextView.textAlignment = View.TEXT_ALIGNMENT_VIEW_END
         }
         else {
             constraintSet.clear(R.id.list_item_message_inner_layout, ConstraintSet.END)
             constraintSet.connect(R.id.list_item_message_inner_layout, ConstraintSet.START, R.id.list_item_message_outer_layout, ConstraintSet.START, 15)
+            messageBoxCardView.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            view.findViewById<LinearLayout>(R.id.list_item_message_inner_layout).gravity = Gravity.START
+            usernameTextView.visibility = View.VISIBLE
+            usernameTextView.setTextColor(Color.parseColor("#000000"))
+            dateTextView.textAlignment = View.TEXT_ALIGNMENT_VIEW_START
         }
 
         constraintSet.applyTo(view)
