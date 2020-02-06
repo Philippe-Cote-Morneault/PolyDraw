90d32878a0c3ed07517aadbdbca527e1ec3bd727
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-05 22:23:10 -0500

    Added logout method

--- Mobile/app/src/main/java/com/log3900/MainActivity.kt
+++ Mobile/app/src/main/java/com/log3900/MainActivity.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import com.log3900.login.LoginActivity
 import androidx.appcompat.app.AppCompatActivity
+import com.log3900.socket.SocketService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // TODO: link to logout button
+    private fun logout() {
+        intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        // TODO: Reset views? Needs test
+        SocketService.instance.disconnect()
+        finish()
     }
 }
