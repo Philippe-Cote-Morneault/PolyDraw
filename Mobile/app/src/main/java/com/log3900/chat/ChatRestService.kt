37858b132993858ecaa40cb6afa277d7b5fd7a38
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-08 14:53:50 -0500

    Added singleton to ChatRestService.

--- Mobile/app/src/main/java/com/log3900/chat/ChatRestService.kt
+++ Mobile/app/src/main/java/com/log3900/chat/ChatRestService.kt
@@ -1,5 +1,6 @@
 package com.log3900.chat
 
+import com.log3900.shared.network.rest.Retrofit
 import com.squareup.moshi.Json
 import retrofit2.Call
 import retrofit2.http.GET
@@ -16,7 +17,11 @@ interface ChatRestService {
     @GET("chat/messages/{channelID}/?start={start}&end={end}")
     fun getChannelMessages(@Header("SessionToken") sessionToken: String, @Header("Language") language: String,
                            @Path("channelID") channelID: String, @Path("start") start: Int, @Path("end") end: Int): Call<ChannelMessages>
-    
+
+    companion object {
+        val service = Retrofit.retrofit.create(ChatRestService::class.java)
+    }
+
 }
 
 data class ChannelMessages(@Json(name = "Messages") var messages: Array<ReceivedMessage>, @Json(name = "MessagesTottal") var messagesTotal: Int)
