55541b7a4c65238bdfa117d8ba77f93c81e038df
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-08 16:12:23 -0500

    Added retrofit singleton.

---
+++ Mobile/app/src/main/java/com/log3900/shared/network/rest/Retrofit.kt
@@ -0,0 +1,23 @@
+package com.log3900.shared.network.rest
+
+import com.log3900.utils.format.moshi.TimeStampAdapter
+import com.log3900.utils.format.moshi.UUIDAdapter
+import com.squareup.moshi.Moshi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+object Retrofit {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("http://log3900.fsae.polymtl.ca:5010/")
+        .addConverterFactory(MoshiConverterFactory.create(createMoshi()))
+        .build()
+
+    fun createMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory())
+            .add(UUIDAdapter())
+            .add(TimeStampAdapter())
+            .build()
+    }
+}
