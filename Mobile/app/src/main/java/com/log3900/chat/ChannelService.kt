28b95a1dec8c51008cee7c2a718c968d28b46bb8
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-08 14:53:15 -0500

    Added ChannelService.

---
+++ Mobile/app/src/main/java/com/log3900/chat/ChannelService.kt
@@ -0,0 +1,30 @@
+package com.log3900.chat
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class ChannelService {
+
+    fun getChannels(token: String): Array<Channel>? {
+        var channels: Array<Channel>? = null
+        val res = ChatRestService.service.getChannels(token, "EN")
+        res.enqueue(object : Callback<Array<Channel>> {
+            override fun onResponse(call: Call<Array<Channel>>, response: Response<Array<Channel>>) {
+                when(response.code()) {
+                    200 -> channels = response.body()
+                    else -> println(response.errorBody().string())
+                }
+            }
+
+            override fun onFailure(call: Call<Array<Channel>>, t: Throwable) {
+                println(t.toString())
+            }
+        })
+
+        return channels
+    }
+
+}
+
