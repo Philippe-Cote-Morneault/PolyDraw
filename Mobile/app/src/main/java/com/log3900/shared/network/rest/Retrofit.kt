71fb2f548806894fc2063662a968720ff3543850
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-08 17:08:38 -0500

    Changed retrofit to use gson.

--- Mobile/app/src/main/java/com/log3900/shared/network/rest/Retrofit.kt
+++ Mobile/app/src/main/java/com/log3900/shared/network/rest/Retrofit.kt
@@ -4,13 +4,14 @@ import com.log3900.utils.format.moshi.TimeStampAdapter
 import com.log3900.utils.format.moshi.UUIDAdapter
 import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 object Retrofit {
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("http://log3900.fsae.polymtl.ca:5010/")
-        .addConverterFactory(MoshiConverterFactory.create(createMoshi()))
+        .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     private fun createMoshi(): Moshi {
