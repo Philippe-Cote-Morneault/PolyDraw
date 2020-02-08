e895d42f154d2f3cad5ce0a007e2f00ab18a334d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-08 13:15:14 -0500

    Updated Channel mode object to have JSON annotations

--- Mobile/app/src/main/java/com/log3900/chat/Channel.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel.kt
@@ -1,5 +1,6 @@
 package com.log3900.chat
 
+import com.squareup.moshi.Json
 import java.util.*
 
-class Channel(var channelName: String, var ID: UUID)
+class Channel(@Json(name = "ID") var ID: UUID, @Json(name = "Name") var name: String, @Json(name = "Users") var users: Array<UUID>)
