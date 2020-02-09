40c747afef5a698eb7ee679de6af0cf5b06a7129
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-02-09 15:42:36 -0500

    Added whitelist for route bypass auth

--- Server/internal/rest/router.go
+++ Server/internal/rest/router.go
@@ -7,8 +7,16 @@ import (
 	"gitlab.com/jigsawcorp/log3900/pkg/rbody"
 )
 
+var authExceptions []string
+
 // setRouters sets the all required routers
 func (a *Server) setRouters() {
+
+	authExceptions = []string{
+		"/healthcheck",
+		"/auth",
+	}
+
 	a.Head("/healthcheck", a.handleRequest(api.HeadHealthcheck))
 	a.Post("/auth", a.handleRequest(api.PostAuth))
 	a.Get("/users", a.handleRequest(api.GetUsers))
