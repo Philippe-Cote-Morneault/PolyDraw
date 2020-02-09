2f6ed209e7a3e1c74e5540c723b8562e6d6de307
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-09 17:50:15 -0500

    Presenter will fetch messages for chat on each resume.

--- Mobile/app/src/main/java/com/log3900/chat/ChatPresenter.kt
+++ Mobile/app/src/main/java/com/log3900/chat/ChatPresenter.kt
@@ -2,17 +2,26 @@ package com.log3900.chat
 
 import android.os.Handler
 import android.os.Message
+import com.log3900.shared.architecture.EventType
+import com.log3900.shared.architecture.MessageEvent
 import com.log3900.shared.architecture.Presenter
 import com.log3900.user.UserRepository
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 import java.util.*
 
 class ChatPresenter : Presenter {
     private var chatView: ChatView
     private var messageRepository: MessageRepository
+    private var chatManager: ChatManager
+    private var keyboardOpened: Boolean
 
     constructor(chatView: ChatView) {
         this.chatView = chatView
+        chatManager = ChatManager()
         messageRepository = MessageRepository.instance!!
+        keyboardOpened = false
 
         subscribeToEvents()
     }
@@ -34,23 +43,49 @@ class ChatPresenter : Presenter {
         }
     }
 
+    fun onKeyboardChange(opened: Boolean) {
+        if (opened != keyboardOpened) {
+            keyboardOpened = opened
+            if (keyboardOpened) {
+                chatView.scrollMessage()
+            }
+        }
+    }
+
     private fun subscribeToEvents() {
         messageRepository.subscribe(MessageRepository.Event.MESSAGE_RECEIVED, Handler {
             handleNewMessage(it)
             true
         })
+
+        EventBus.getDefault().register(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: MessageEvent) {
+        when(event.type) {
+            EventType.CHANGED_CHANNEL -> {
+                onChannelChanged(event.data as Channel)
+            }
+        }
+    }
+
+    private fun onChannelChanged(channel: Channel) {
+        chatView.setCurrentChannnelName(channel.name)
     }
 
     private fun handleNewMessage(message: Message) {
         val receivedMessage = message.obj as ReceivedMessage
-        chatView.appendMessage(receivedMessage)
+        chatView.notifyNewMessage()
         if (UserRepository.getUser().username != receivedMessage.senderName) {
-            chatView.notifyNewMessage()
+            chatView.playNewMessageNotification()
         }
     }
 
     override fun resume() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        chatManager.test().subscribe {
+            chatView.setReceivedMessages(it!!)
+        }
     }
 
     override fun pause() {
