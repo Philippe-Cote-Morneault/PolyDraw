2c3a3f7a486713e015d989d8b037e9235d32a88a
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-09 18:05:40 -0500

    Added ChannelList default layout classes.

---
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListPresenter.kt
@@ -0,0 +1,17 @@
+package com.log3900.chat.Channel
+
+import com.log3900.shared.architecture.Presenter
+
+class ChannelListPresenter(var channelListView: ChannelListView) : Presenter {
+    override fun resume() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pause() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun destroy() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
