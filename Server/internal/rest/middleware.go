3ebb0248bef05f4659bc2a7cb39ca59de9b0722a
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-02-09 15:41:05 -0500

    Created a middleware for the auth

--- Server/internal/rest/middleware.go
+++ Server/internal/rest/middleware.go
@@ -3,6 +3,10 @@ package rest
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/context"
+	"gitlab.com/jigsawcorp/log3900/internal/services/auth"
+	"gitlab.com/jigsawcorp/log3900/pkg/rbody"
 )
 
 type statusWriter struct {
@@ -31,4 +35,35 @@ func logMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(&sw, r)
 		log.Printf("[REST] %d - %s %s", sw.status, r.Method, r.RequestURI)
 	})
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !stringInSlice(r.URL.Path, authExceptions) {
+			sessionToken := r.Header.Get("SessionToken")
+			if sessionToken != "" {
+				//There is a token, check if it's valid and return the userID
+				ok, userID := auth.GetUserIDFromToken(sessionToken)
+				if !ok {
+					rbody.JSONError(w, http.StatusForbidden, "The header SessionToken is invalid.")
+				} else {
+					context.Set(r, "user", userID)
+					next.ServeHTTP(w, r)
+				}
+			} else {
+				rbody.JSONError(w, http.StatusForbidden, "The header SessionToken is invalid.")
+			}
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
