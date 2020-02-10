85e51e141e30b2d0281e50587f9da317b4899ac5
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-09 23:13:55 -0500

    Changed ViewHolders to match new recyclerview library.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelGroupViewHolder.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelGroupViewHolder.kt
@@ -2,11 +2,10 @@ package com.log3900.chat.Channel
 
 import android.view.View
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.log3900.R
-import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder
-import kotlinx.android.synthetic.main.list_item_channel_group.view.*
 
-class ChannelGroupViewHolder : GroupViewHolder {
+class ChannelGroupViewHolder : RecyclerView.ViewHolder {
     private var channelGroupNameTextView: TextView
     constructor(itemView: View) : super(itemView) {
         channelGroupNameTextView = itemView.findViewById(R.id.list_item_channel_group_name)
--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListFragment.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListFragment.kt
@@ -5,19 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.log3900.R
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter
+import java.util.*
 
 class ChannelListFragment : Fragment(), ChannelListView {
     // Services
     private lateinit var channelListPresenter: ChannelListPresenter
     // UI elements
-
+    private lateinit var channelsRecyclerView: RecyclerView
+    private lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView: View = inflater.inflate(R.layout.fragment_channel_list, container, false)
 
-        channelListPresenter = ChannelListPresenter(this)
+        var channelGroups = arrayListOf<ChannelGroup>()
+        var channels1 = arrayListOf<Channel>()
+        channels1.add(Channel(UUID.randomUUID(), "channel1", arrayOf()))
+        channels1.add(Channel(UUID.randomUUID(), "channel2", arrayOf()))
+        channels1.add(Channel(UUID.randomUUID(), "channel3", arrayOf()))
+        var channels2 = arrayListOf<Channel>()
+        channels2.add(Channel(UUID.randomUUID(), "channel4", arrayOf()))
+        channels2.add(Channel(UUID.randomUUID(), "channel5", arrayOf()))
+        channels2.add(Channel(UUID.randomUUID(), "channel6", arrayOf()))
+        channelGroups.add(ChannelGroup("ChannelGroup1", channels1))
+        channelGroups.add(ChannelGroup("ChannelGroup2", channels2))
 
+        channelsRecyclerView = rootView.findViewById(R.id.fragment_channel_list_recycler_view_channels)
+        layoutManager = LinearLayoutManager(this.context)
+        channelsRecyclerView.layoutManager = layoutManager
+        val channelAdapter = SectionedRecyclerViewAdapter()
+        channelAdapter.addSection(ChannelSection(channels1))
+        channelAdapter.addSection(ChannelSection(channels2))
+        channelListPresenter = ChannelListPresenter(this)
+        channelsRecyclerView.adapter = channelAdapter
         return rootView
     }
 }
