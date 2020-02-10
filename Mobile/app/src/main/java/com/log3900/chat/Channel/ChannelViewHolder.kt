3a2e8c258357ac7382fbe75b26f2715a5cba0b4d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-09 22:13:47 -0500

    Created viewHolder for channels.

---
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelViewHolder.kt
@@ -0,0 +1,17 @@
+package com.log3900.chat.Channel
+
+import android.view.View
+import android.widget.TextView
+import com.log3900.R
+import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder
+
+class ChannelViewHolder : ChildViewHolder {
+    private var channelName: TextView
+    constructor(itemView: View) : super(itemView){
+        channelName = itemView.findViewById(R.id.list_item_channel_name)
+    }
+    
+    fun onBind(channel: Channel) {
+        channelName.setText(channel.name)
+    }
+}
