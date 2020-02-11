b3b24aa859c52d9089f7da82598d42151ab8ec47
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 18:21:26 -0500

    CHannelListPresenter waits until ChatManager is ready.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListPresenter.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListPresenter.kt
@@ -1,8 +1,33 @@
 package com.log3900.chat.Channel
 
+import com.log3900.chat.ChatManager
 import com.log3900.shared.architecture.Presenter
+import com.log3900.shared.ui.ProgressDialog
 
-class ChannelListPresenter(var channelListView: ChannelListView) : Presenter {
+class ChannelListPresenter : Presenter {
+    private var channelListView: ChannelListView
+    private lateinit var chatManager: ChatManager
+
+    constructor(channelListView: ChannelListView) {
+        this.channelListView = channelListView
+        if (!(ChatManager.instance?.ready!!)) {
+            ChatManager.instance?.subject?.filter {
+                it
+            }?.subscribe {
+                init()
+            }
+        } else {
+            init()
+        }
+    }
+
+    private fun init() {
+        this.chatManager = ChatManager.instance!!
+    }
+
+    fun onChannelClicked(channel: Channel) {
+        chatManager.setActiveChannel(channel)
+    }
     override fun resume() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
