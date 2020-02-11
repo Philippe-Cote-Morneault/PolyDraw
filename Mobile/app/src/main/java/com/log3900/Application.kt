59b00c80ad4c8b7b5a0f1fe62f8ecd09e424a386
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 15:21:43 -0500

    Added ChatManager as a service.

--- Mobile/app/src/main/java/com/log3900/Application.kt
+++ Mobile/app/src/main/java/com/log3900/Application.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
 import com.log3900.chat.Channel.ChannelRepository
+import com.log3900.chat.ChatManager
 import com.log3900.chat.Message.MessageRepository
 import com.log3900.session.MonitoringService
 import com.log3900.socket.SocketService
@@ -14,13 +15,6 @@ import com.log3900.socket.SocketService
 class MainApplication : Application() {
     companion object {
         lateinit var instance: MainApplication
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        instance = this
-
         val serviceConnection1 = object : ServiceConnection {
             override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             }
@@ -28,6 +22,12 @@ class MainApplication : Application() {
             override fun onServiceDisconnected(name: ComponentName?) {
             }
         }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
 
         bindService(Intent(this, SocketService::class.java), serviceConnection1, Context.BIND_AUTO_CREATE)
 
@@ -36,5 +36,9 @@ class MainApplication : Application() {
         bindService(Intent(this, MessageRepository::class.java), serviceConnection1, Context.BIND_AUTO_CREATE)
 
         bindService(Intent(this, ChannelRepository::class.java), serviceConnection1, Context.BIND_AUTO_CREATE)
+    }
+
+    fun startService(service: Class<*>) {
+        bindService(Intent(this, service), serviceConnection1, Context.BIND_AUTO_CREATE)
     }
 }
