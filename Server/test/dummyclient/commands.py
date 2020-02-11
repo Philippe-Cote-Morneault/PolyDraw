df583a878cdb7434b1ac266db2a4ec93a48440dc
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-02-10 23:55:04 -0500

    Added a bunch of example commented out

--- Server/test/dummyclient/commands.py
+++ Server/test/dummyclient/commands.py
@@ -1,7 +1,9 @@
 import msgpack
 import uuid
+import time
 
 sendMessage = None
+uuidNul = "00000000-0000-0000-0000-000000000000"
 
 def load(funcSendMessage):
     global sendMessage
@@ -9,6 +11,38 @@ def load(funcSendMessage):
 
 def execute():
     # You can add anything to reload
+    #Example message
+    """
+    #Send message
     sendMessage(20, msgpack.packb(
-        {"Message":"Hello ca va", "ChannelID":str(uuid.uuid4())}
+        {"Message":"Hello ca va", "ChannelID":uuidNul}
     ))
+    """
+    """
+    #Create channel
+    sendMessage(26, msgpack.packb(
+        {"ChannelName":"Boby"}
+    ))
+    """
+    """
+    #Destroy channel
+    sendMessage(28, uuid.UUID("7040b302-6b1e-40f0-b9e8-12ee7301b2bf").bytes)
+    """
+    """
+    #Join channel
+    sendMessage(22, uuid.UUID("7040b302-6b1e-40f0-b9e8-12ee7301b2bf").bytes)
+    """
+    """
+    #Quit channel
+    sendMessage(24, uuid.UUID("7040b302-6b1e-40f0-b9e8-12ee7301b2bf").bytes)
+    """
+    """
+    #Send message
+    sendMessage(20, msgpack.packb(
+        {"Message":"Hello ca va", "ChannelID":"7040b302-6b1e-40f0-b9e8-12ee7301b2bf"}
+    ))
+    """
+    
+    
+    
+    
