79ea4cf50f491e0febca8b3c26c8d6aa88b1b09a
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 18:17:13 -0500

    Moved sending message functions to MessageManager,

--- Mobile/app/src/main/java/com/log3900/chat/Message/MessageManager.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message/MessageManager.kt
@@ -1,9 +1,21 @@
 package com.log3900.chat.Message
 
+import com.log3900.chat.Channel.Channel
+import com.log3900.user.UserRepository
+import java.util.*
+
 class MessageManager {
     private var messageRepository: MessageRepository
 
     constructor() {
         messageRepository = MessageRepository.instance!!
+    }
+
+    fun getMessages(channel: Channel): LinkedList<ReceivedMessage> {
+        return messageRepository.getChannelMessages(channel.ID.toString(), UserRepository.getUser().sessionToken, 0, 100)
+    }
+
+    fun sendMessage(channelID:UUID, message: String) {
+        messageRepository.sendMessage(SentMessage(message, channelID))
     }
 }
