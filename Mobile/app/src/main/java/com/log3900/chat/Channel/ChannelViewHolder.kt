f130afb8f7e6d6d8d11d28203dcbbae93a9240f8
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 22:27:10 -0500

    Added button to ChannelViewHolder.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelViewHolder.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelViewHolder.kt
@@ -3,12 +3,15 @@ package com.log3900.chat.Channel
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import com.log3900.R
 
 class ChannelViewHolder : RecyclerView.ViewHolder {
     private var channelName: TextView
+    var buttonAction1: MaterialButton
     constructor(itemView: View) : super(itemView){
         channelName = itemView.findViewById(R.id.list_item_channel_name)
+        buttonAction1 = itemView.findViewById(R.id.list_item_channel_button_action_1)
     }
 
     fun bind(channel: Channel) {
