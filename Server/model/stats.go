e3660fa1c70a7a89511a885528720d5cce42236a
Allan Beddouk <allan.beddouk@polymtl.ca>
2020-02-12 15:50:45 -0500

    Ajout des fonctions pour update les tables en base de donnees

--- Server/model/stats.go
+++ Server/model/stats.go
@@ -4,17 +4,18 @@ import (
 	"github.com/google/uuid"
 )
 
-//PlayerStats represents the statistics of the player
-type PlayerStats struct {
-	Base               `gorm:"foreignkey:UserID"`
-	UserID             uuid.UUID
-	GamesPlayed        int64
-	WinRatio           float64
-	AvgGameDuration    int64
-	TimePlayed         int64         `gorm:"foreignkey:ConnectionHistory"`
-	ConnectionHistory  []Connection  `gorm:"foreignkey:GamesPlayedHistory"`
-	GamesPlayedHistory []MatchPlayed `gorm:"foreignkey:Achievements"`
-	Achievements       []Achievement
+//UserStats represents the statistics of the player
+type UserStats struct {
+	Base
+	User                 User `gorm:"foreignkey:UserID"`
+	UserID               uuid.UUID
+	GamesPlayed          int64
+	WinRatio             float64
+	AvgGameDuration      int64
+	TimePlayed           int64         `gorm:"foreignkey:ConnectionHistory"`
+	ConnectionHistory    []Connection  `gorm:"foreignkey:MatchesPlayedHistory"`
+	MatchesPlayedHistory []MatchPlayed `gorm:"foreignkey:Achievements"`
+	Achievements         []Achievement
 }
 
 //Connection represents the information of a connection
@@ -28,7 +29,6 @@ type Connection struct {
 type MatchPlayed struct {
 	Base
 	PlayerNames   []string // a voir si on laisse le tableau de nom ou si on met un tableau de joueur (moins couteux string)
-	DatePlayed    int64
 	MatchDuration int64
 	WinnerName    string
 }
@@ -39,4 +39,47 @@ type Achievement struct {
 	TropheeName   string
 	Description   string
 	ObtainingDate int64
+}
+
+// FindStatsByUserID finds stats by userId
+func FindStatsByUserID(userID uuid.UUID, userStats *UserStats) bool {
+	DB().Where("userID = ?", userID).Find(&userStats)
+	return userStats.ID == userID
+}
+
+// UpdateStats updates userStats each time a match/game is finished
+func UpdateStats(userID uuid.UUID, matchPlayed *MatchPlayed, connection *Connection) {
+	var userStats UserStats
+	DB().Where("userID = ?", userID).Find(&userStats)
+
+	var gamesPlayed int64 = userStats.GamesPlayed + 1
+	var timePlayed int64 = userStats.TimePlayed + matchPlayed.MatchDuration
+
+	var winRatio float64 = userStats.WinRatio
+	if matchPlayed.WinnerName == userStats.User.Username {
+		winRatio = (userStats.WinRatio*float64(gamesPlayed) + 1) / float64(gamesPlayed)
+	}
+
+	userStats.ConnectionHistory = append(userStats.ConnectionHistory, *connection)
+	userStats.MatchesPlayedHistory = append(userStats.MatchesPlayedHistory, *matchPlayed)
+
+	DB().Where("userID = ?", userID).Updates(map[string]interface{}{
+		"gamesPlayed":          gamesPlayed,
+		"winRatio":             winRatio,
+		"timePlayed":           timePlayed,
+		"avgGameDuration":      float64(timePlayed) / float64(gamesPlayed),
+		"matchesPlayedHistory": userStats.MatchesPlayedHistory,
+		"connectionHistory":    userStats.ConnectionHistory,
+	})
+}
+
+// AddAchievement will add the achievement to the userStats
+func AddAchievement(userID uuid.UUID, achievement *Achievement) {
+	var userStats UserStats
+	DB().Where("userID = ?", userID).Find(&userStats)
+
+	userStats.Achievements = append(userStats.Achievements, *achievement)
+
+	DB().Where("userID = ?", userID).Update("achievements", userStats.Achievements)
+
 }
