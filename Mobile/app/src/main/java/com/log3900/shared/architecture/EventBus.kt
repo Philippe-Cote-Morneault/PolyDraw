6c67b2d54179ef36fea38d0d7925fb04d6aae075
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 22:54:56 -0500

    Added channel creation/deletion events in the EventBus.

--- Mobile/app/src/main/java/com/log3900/shared/architecture/EventBus.kt
+++ Mobile/app/src/main/java/com/log3900/shared/architecture/EventBus.kt
@@ -3,6 +3,8 @@ package com.log3900.shared.architecture
 enum class EventType {
     ACTIVE_CHANNEL_CHANGED,
     SUBSCRIBED_TO_CHANNEL,
-    UNSUBSCRIBED_FROM_CHANNEL
+    UNSUBSCRIBED_FROM_CHANNEL,
+    CHANNEL_CREATED,
+    CHANNEL_DELETED,
 }
 data class MessageEvent(var type: EventType, var data: Any)
