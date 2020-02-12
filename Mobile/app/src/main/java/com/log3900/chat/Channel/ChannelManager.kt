5777a7c9349ff6e8b01e687401119f36dccfba7d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 21:22:47 -0500

    ChannelManager now keeps channels.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelManager.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelManager.kt
@@ -1,15 +1,25 @@
 package com.log3900.chat.Channel
 
+import com.log3900.chat.ChatManager
 import com.log3900.user.User
 import com.log3900.user.UserRepository
 
 class ChannelManager {
     private var user: User
-    //private var channels: ArrayList<Channel>
+    lateinit var activeChannel: Channel
+    lateinit var availableChannels: ArrayList<Channel>
+    lateinit var joinedChannels: ArrayList<Channel>
 
     constructor() {
         user = UserRepository.getUser()
-        //channels = ChannelRepository.getChannels(user.sessionToken)
+    }
+
+    fun init() {
+        joinedChannels = ChannelRepository.instance?.getJoinedChannels(user.sessionToken)?.blockingGet()!!
+        availableChannels = ChannelRepository.instance?.getAvailableChannels(user.sessionToken)?.blockingGet()!!
+        activeChannel = joinedChannels.find {
+            it.ID.toString() == "00000000-0000-0000-0000-000000000000"
+        }!!
     }
 
 
