8833b1aaeb8cbbdab9e0b70e1edf7fccfb46a093
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-11 21:22:17 -0500

    Added new functions to ChannelListView interface.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListView.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListView.kt
@@ -2,4 +2,6 @@ package com.log3900.chat.Channel
 
 interface ChannelListView {
     //fun showChannel
+    fun setJoinedChannels(channels: ArrayList<Channel>)
+    fun setAvailableChannels(channels: ArrayList<Channel>)
 }
