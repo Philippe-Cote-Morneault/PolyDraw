59ae2eea55398ef23601d32634bdc2198cdefc3b
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-12 09:50:01 -0500

    Corrected profile databinding

--- Mobile/app/src/main/java/com/log3900/profile/ProfileFragment.kt
+++ Mobile/app/src/main/java/com/log3900/profile/ProfileFragment.kt
@@ -5,11 +5,9 @@ import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.log3900.R
-import com.log3900.databinding.FragmentProfileInfoBinding
 import com.log3900.profile.models.ProfileInfo
 
 class ProfileFragment : Fragment() {
@@ -17,15 +15,12 @@ class ProfileFragment : Fragment() {
     private val profileVm: ProfileViewModel by lazy {
         ViewModelProviders.of(this).get(ProfileViewModel::class.java)
     }
-    private lateinit var infoBinding: FragmentProfileInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        infoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_info, container, false)
-        infoBinding.info = ProfileInfo("myusername", "mypassword123", "Myname", "My-Lastname", "my@email.com")
 //
 //        val contextThemeWrapper = ContextThemeWrapper(activity, R.style.MyTheme_DayNight)
 //        val localInflater = inflater.cloneInContext(contextThemeWrapper)
--- Mobile/app/src/main/java/com/log3900/profile/ProfileInfoFragment.kt
+++ Mobile/app/src/main/java/com/log3900/profile/ProfileInfoFragment.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.log3900.R
+import com.log3900.databinding.FragmentProfileInfoBinding
+import com.log3900.profile.models.ProfileInfo
 
 class ProfileInfoFragment : Fragment() {
+    private lateinit var infoBinding: FragmentProfileInfoBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        println("hello from fragment")
-        val root = inflater.inflate(R.layout.fragment_profile_info, container, false)
-        return root
+        infoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_info, container, false)
+        infoBinding.info = ProfileInfo("myusername", "mypassword123", "Myname", "My-Lastname", "my@email.com")
+
+        return infoBinding.root
     }
 }
