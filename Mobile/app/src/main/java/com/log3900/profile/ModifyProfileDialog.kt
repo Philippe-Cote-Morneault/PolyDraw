6349a573396988eb4aadd73c0b14f45396843b24
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-12 15:35:26 -0500

    Stash merge

--- Mobile/app/src/main/java/com/log3900/profile/ModifyProfileDialog.kt
+++ Mobile/app/src/main/java/com/log3900/profile/ModifyProfileDialog.kt
@@ -7,9 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.log3900.R
+import com.log3900.user.Account
+import com.log3900.user.AccountRepository
 
 class ModifyProfileDialog : DialogFragment() {
+    lateinit var originalAccount: Account
 
     override fun onStart() {
         super.onStart()
@@ -27,6 +32,7 @@ class ModifyProfileDialog : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.dialog_modify_profile, container, false)
+        originalAccount = AccountRepository.getAccount()
         setUpUi(rootView)
         return rootView
     }
@@ -36,5 +42,16 @@ class ModifyProfileDialog : DialogFragment() {
         cancelBtn.setOnClickListener {
             dismiss()
         }
+
+        fillDefaultDialogFields(root)
+    }
+
+    /**
+     * Fills the dialog with current account info
+     */
+    private fun fillDefaultDialogFields(root: View) {
+        // TODO: Avatar
+        val usernameInput = root.findViewById<TextInputEditText>(R.id.username_input)
+        usernameInput.setText(originalAccount.username)
     }
 }
