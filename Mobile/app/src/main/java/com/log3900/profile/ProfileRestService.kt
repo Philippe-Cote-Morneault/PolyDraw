b9ad31a531634352edf45d72a17597da43a6b4bf
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-15 18:38:52 -0500

    Added get stats request in Profile service

--- Mobile/app/src/main/java/com/log3900/profile/ProfileRestService.kt
+++ Mobile/app/src/main/java/com/log3900/profile/ProfileRestService.kt
@@ -4,17 +4,23 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.log3900.shared.network.rest.Retrofit
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 interface ProfileRestService {
 
+    // Profile modification
     @PUT("users/")
     fun modifyProfile(
         @Header("SessionToken") sessionToken: String, @Header("Language") language: String,
         @Body data: JsonObject
     ): Call<JsonArray>
+
+    // Statistics
+    @GET("stats/{userid}")
+    fun getStats(
+        @Header("SessionToken") sessionToken: String, @Header("Language") language: String,
+        @Path("userid") userId: String
+    ): Call<JsonObject>
 
     companion object {
         val service = Retrofit.retrofit.create(ProfileRestService::class.java)
