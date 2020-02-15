3863a9374861731e92de49c4deb99e800b2e92c7
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-15 18:26:41 -0500

    Added stats presenter for profile

--- Mobile/app/src/main/java/com/log3900/profile/stats/ProfileStatsFragment.kt
+++ Mobile/app/src/main/java/com/log3900/profile/stats/ProfileStatsFragment.kt
@@ -5,15 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.log3900.R
 
 class ProfileStatsFragment : Fragment() {
+
+    val profileStatsPresenter = ProfileStatsPresenter(this)
+    lateinit var connectionHistoryButton: MaterialButton
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_profile_stats, container, false)
+
+        setUpUI(root)
+
         return root
+    }
+
+    private fun setUpUI(root: View) {
+        connectionHistoryButton = root.findViewById(R.id.connection_history_button)
+        connectionHistoryButton.setOnClickListener {
+            MaterialAlertDialogBuilder(activity)
+                .setMessage("clicked")
+                .show()
+        }
+
+        fetchStats()
+    }
+
+    private fun fetchStats() {
+        profileStatsPresenter.fetchStats()
     }
 }
---
+++ Mobile/app/src/main/java/com/log3900/profile/stats/ProfileStatsPresenter.kt
@@ -0,0 +1,14 @@
+package com.log3900.profile.stats
+
+import io.reactivex.Single
+
+class ProfileStatsPresenter(val statsView: ProfileStatsFragment) {
+
+    fun fetchStats() {
+
+    }
+
+    private fun sendStatsRequest(): Single<UserStats> {
+
+    }
+}
