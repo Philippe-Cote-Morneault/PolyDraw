04031479f38914926b790c5af36c08a47aa5446e
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-15 18:33:03 -0500

    Basic request handling for stats presenter

--- Mobile/app/src/main/java/com/log3900/profile/stats/ProfileStatsPresenter.kt
+++ Mobile/app/src/main/java/com/log3900/profile/stats/ProfileStatsPresenter.kt
@@ -1,14 +1,31 @@
 package com.log3900.profile.stats
 
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class ProfileStatsPresenter(val statsView: ProfileStatsFragment) {
 
     fun fetchStats() {
-
+        sendStatsRequest()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ userStats ->
+                onStatsFetchSuccess(userStats)
+            }, { error ->
+                onStatsFetchError(error.message)
+            })
     }
 
     private fun sendStatsRequest(): Single<UserStats> {
+        return Single.create {
+            // TODO: Do call
+        }
+    }
+
+    private fun onStatsFetchSuccess(userStats: UserStats) {
+
+    }
+
+    private fun onStatsFetchError(error: String?) {
 
     }
 }
