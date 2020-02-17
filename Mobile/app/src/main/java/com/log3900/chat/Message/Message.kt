47251112f8ce44dec71ecf3faf7e7adec4ec6bf8
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-16 20:32:21 -0500

    Added EventMessage type.

--- Mobile/app/src/main/java/com/log3900/chat/Message/Message.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message/Message.kt
@@ -3,7 +3,10 @@ package com.log3900.chat.Message
 import com.squareup.moshi.Json
 import java.util.*
 
-data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  UUID, @Json(name = "UserID") var userID: UUID,
-                           @Json(name = "Username") var username: String, @Json(name = "Timestamp") var timestamp: Date)
+data class ReceivedMessage(@Json(name = "Message") var message: String, @Json(name = "ChannelID") var channelID:  UUID,
+                           @Json(name = "UserID") var userID: UUID, @Json(name = "Username") var username: String,
+                           @Json(name = "Timestamp") var timestamp: Date)
 
 data class SentMessage(@Json(name = "Message")var message: String, @Json(name = "ChannelID") var channelID: UUID)
+
+data class EventMessage(var message: String)
