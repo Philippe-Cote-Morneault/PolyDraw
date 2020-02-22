23d71770db4aa0606b359b941d4585dd4abe60df
cedric <cedric.tessier@polymtl.ca>
2020-02-22 12:21:57 -0500

    Added the upload image command

--- Desktop/Projet3/ClientLourd/ViewModels/GameCreationViewModel.cs
+++ Desktop/Projet3/ClientLourd/ViewModels/GameCreationViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Documents;
+using System.Windows.Input;
 using ClientLourd.Services.EnumService;
+using ClientLourd.Utilities.Commands;
 using ClientLourd.Utilities.Enums;
 
 namespace ClientLourd.ViewModels
@@ -13,8 +15,7 @@ namespace ClientLourd.ViewModels
         public GameCreationViewModel()
         {
             Hints = new ObservableCollection<string>(new string[3]);
-            Hints.CollectionChanged += (sender, args) => { NotifyPropertyChanged(nameof(AreFieldsEmpty)); }; 
-            NotifyPropertyChanged(nameof(SelectedMode));
+            Hints.CollectionChanged += (sender, args) => { NotifyPropertyChanged(nameof(AreFieldsEmpty)); };
         }
         
         public override void AfterLogOut() { throw new System.NotImplementedException(); }
@@ -63,6 +64,54 @@ namespace ClientLourd.ViewModels
         public List<string> PotraceModes
         {
             get { return EnumManager.GetAllDescriptions<PotraceMode>(); }
-        }  
+        }
+
+        public string Image
+        {
+            get { return _image; }
+            set
+            {
+                if (_image != value)
+                {
+                    _image = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged(nameof(IsImageUpload));
+                }
+            }
+        }
+
+        private string _image;
+        public bool IsImageUpload
+        {
+            get { return !string.IsNullOrWhiteSpace(_image); }
+        }
+        
+        RelayCommand<string> _uploadImageCommand;
+
+        public ICommand UploadImageCommand
+        {
+            get
+            {
+                return _uploadImageCommand ??
+                       (_uploadImageCommand = new RelayCommand<string>(image => UploadImage(image)));
+            }
+        }
+        
+        RelayCommand<object> _removeImageCommand;
+        public ICommand RemoveImageCommand
+        {
+            get
+            {
+                return _removeImageCommand ??
+                       (_removeImageCommand = new RelayCommand<object>(image => Image = ""));
+            }
+        }
+
+        private void UploadImage(string image)
+        {
+            //TODO validate the file
+            Image = image;
+        }
+        
     }
 }
--- Desktop/Projet3/ClientLourd/Views/Dialogs/GameCreationDialog.xaml.cs
+++ Desktop/Projet3/ClientLourd/Views/Dialogs/GameCreationDialog.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using ClientLourd.ViewModels;
+using Microsoft.Win32;
 
 namespace ClientLourd.Views.Dialogs
 {
@@ -7,6 +10,26 @@ namespace ClientLourd.Views.Dialogs
         public GameCreationDialog()
         {
             InitializeComponent();
+        }
+
+        private void UploadImageClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //TODO update the filter
+            openFileDialog.Filter =  "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png *.bmp) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png *.bmp";
+            if (openFileDialog.ShowDialog() == true)
+            { 
+                ((GameCreationViewModel) DataContext).UploadImageCommand.Execute(openFileDialog.FileName);
+            }
+        }
+
+        private void DropUpload(object sender, DragEventArgs e)
+        {
+              if (e.Data.GetDataPresent(DataFormats.FileDrop))
+              {
+                  string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                  if (files != null) ((GameCreationViewModel) DataContext).UploadImageCommand.Execute(files[0]);
+              }
         }
     }
 }
