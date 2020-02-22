5278d2c26561d7cdbb5dbd5dace549dd4085c5bb
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-02-22 12:43:07 -0500

    Added match history to profile with matches played

--- Mobile/app/src/main/java/com/log3900/profile/matchhistory/ProfileMatchHistoryFragment.kt
+++ Mobile/app/src/main/java/com/log3900/profile/matchhistory/ProfileMatchHistoryFragment.kt
@@ -5,7 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.log3900.R
+import com.log3900.profile.stats.GamePlayed
+import com.log3900.profile.stats.StatsRepository
+import com.log3900.user.AccountRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ProfileMatchHistoryFragment : Fragment() {
 
@@ -22,6 +32,28 @@ class ProfileMatchHistoryFragment : Fragment() {
     }
 
     private fun setUpUI(root: View) {
-
+        setUpMatchesPlayedRecyclerView(root)
     }
+
+    private fun setUpMatchesPlayedRecyclerView(root: View) {
+        GlobalScope.launch {
+            withContext(Dispatchers.Main) {
+                val rvMatches: RecyclerView = root.findViewById(R.id.rv_matches)
+                val matches = getMatchesPlayed()
+                val matchesAdapter = MatchPlayedAdapter(matches, AccountRepository.getAccount().username)
+
+                rvMatches.apply {
+                    adapter = matchesAdapter
+                    layoutManager = LinearLayoutManager(activity)
+                    addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+                }
+            }
+        }
+    }
+
+    private suspend fun getMatchesPlayed(): List<GamePlayed> {
+        // TODO: Error handling
+        return StatsRepository.getGamesPlayedHistory()
+    }
+
 }
