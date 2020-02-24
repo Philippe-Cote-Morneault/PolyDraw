63b879f93c2b2546395cf1ec10e6d8b01959c986
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-24 16:42:45 -0500

    Converted UserCache to map Users by ID instead of Username.

--- Mobile/app/src/main/java/com/log3900/user/UserCache.kt
+++ Mobile/app/src/main/java/com/log3900/user/UserCache.kt
@@ -1,23 +1,27 @@
 package com.log3900.user
 
+import java.util.*
+import kotlin.NoSuchElementException
+import kotlin.collections.HashMap
+
 class UserCache {
-    private var users: HashMap<String, User> = HashMap()
+    private var users: HashMap<UUID, User> = HashMap()
 
     fun addUser(user: User) {
-        if (!users.containsKey(user.username)) {
-            users[user.username] = user
+        if (!users.containsKey(user.ID)) {
+            users[user.ID] = user
         }
     }
 
-    fun getUser(username: String): User {
-        if (containsUser(username)) {
-            return users[username]!!
+    fun getUser(userID: UUID): User {
+        if (containsUser(userID)) {
+            return users[userID]!!
         }
 
-        throw NoSuchElementException("User $username is not present in cache.")
+        throw NoSuchElementException("User $userID is not present in cache.")
     }
 
-    fun containsUser(username: String): Boolean {
-        return users.containsKey(username)
+    fun containsUser(userID: UUID): Boolean {
+        return users.containsKey(userID)
     }
 }
