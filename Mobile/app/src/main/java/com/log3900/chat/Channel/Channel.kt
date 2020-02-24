2ec7ed68208d7dd68ab5ced5bd9e64fe4fdcffe9
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-24 18:42:14 -0500

    Changed Channel to hold UUID instead of User.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/Channel.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/Channel.kt
@@ -4,7 +4,7 @@ import com.log3900.user.User
 import com.squareup.moshi.Json
 import java.util.*
 
-class Channel(@Json(name = "ID") var ID: UUID, @Json(name = "Name") var name: String, @Json(name = "Users") var users: Array<User>) {
+class Channel(@Json(name = "ID") var ID: UUID, @Json(name = "Name") var name: String, @Json(name = "Users") var users: Array<UUID>) {
     companion object {
         val GENERAL_CHANNEL_ID: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
     }
