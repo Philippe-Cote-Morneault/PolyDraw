e345ccd5c497138067c0e4831cbae0e6671e7af0
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-25 18:48:17 -0500

    ChannelListView now has confirmation dialog methods.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListView.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListView.kt
@@ -1,5 +1,7 @@
 package com.log3900.chat.Channel
 
+import android.content.DialogInterface
+
 interface ChannelListView {
     fun setJoinedChannels(channels: ArrayList<Channel>)
     fun setAvailableChannels(channels: ArrayList<Channel>)
@@ -7,5 +9,8 @@ interface ChannelListView {
     fun notifyChannelUnsubscried(channel: Channel)
     fun showChannelCreationDialog(positiveCallback: (channelName: String) -> Unit)
     fun hideChannelCreationDialog()
+    fun showConfirmationDialog(title: String, message: String, positiveButtonListener: ((dialog: DialogInterface, which: Int) -> Unit),
+                               negativeButtonListener: ((dialog: DialogInterface, which: Int) -> Unit))
+    fun hideConfirmationDialog()
     fun notifyChannelsChange()
 }
