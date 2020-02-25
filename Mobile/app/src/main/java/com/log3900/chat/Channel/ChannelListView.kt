4f6b7f6aab0afe9aa3d41fc77d6171e9943de34a
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-25 17:51:16 -0500

    Removed useless method in ChannelListView interface.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListView.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelListView.kt
@@ -1,7 +1,6 @@
 package com.log3900.chat.Channel
 
 interface ChannelListView {
-    //fun showChannel
     fun setJoinedChannels(channels: ArrayList<Channel>)
     fun setAvailableChannels(channels: ArrayList<Channel>)
     fun notifyChannelSubscribed(channel: Channel)
