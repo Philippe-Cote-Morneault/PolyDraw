d76ac4b241bab631e83ea9ce9a26195f232f3511
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-02-28 00:48:04 -0500

    Implemented the dummy stroke and the marshall

--- Server/internal/services/drawing/handles.go
+++ Server/internal/services/drawing/handles.go
@@ -1,10 +1,27 @@
 package drawing
 
 import (
+	"encoding/binary"
 	"github.com/google/uuid"
 	"gitlab.com/jigsawcorp/log3900/internal/socket"
 	"gitlab.com/jigsawcorp/log3900/model"
+	"gitlab.com/jigsawcorp/log3900/pkg/geometry"
 )
+
+const MaxUint16 = ^uint16(0)
+
+type point geometry.Point
+
+type Stroke struct {
+	ID        uuid.UUID
+	color     byte
+	isEraser  bool
+	isSquared bool
+	width     uint16
+	height    uint16
+	brushSize byte
+	points    []point
+}
 
 func (d *Drawing) handlePreview(message socket.RawMessageReceived) {
 	drawingID, err := uuid.FromBytes(message.Payload.Bytes)
@@ -29,7 +46,7 @@ func (d *Drawing) handlePreview(message socket.RawMessageReceived) {
 	}, message.SocketID)
 	//Call method to start the drawing here
 	//TODO Allan your code goes here!
-
+	sendDummyDrawing(message.SocketID)
 	socket.SendRawMessageToSocketID(socket.RawMessage{
 		MessageType: byte(socket.MessageType.EndDrawingServer),
 		Length:      uint16(len(uuidBytes)),
@@ -51,4 +68,99 @@ func sendPreviewResponse(socketID uuid.UUID, response bool) {
 		Bytes:       bytesResponse,
 	}
 	socket.SendRawMessageToSocketID(packet, socketID)
+}
+
+func sendDummyDrawing(socketID uuid.UUID) {
+	fakeStroke := Stroke{
+		ID:        uuid.New(),
+		color:     0x02,
+		isEraser:  false,
+		isSquared: true,
+		width:     1920,
+		height:    1080,
+		brushSize: 25,
+	}
+	points := []point{
+		{0, 0},
+		{5, 5},
+		{10, 10},
+		{15.5, 15.5},
+	}
+	fakeStroke.points = points
+
+	payload := fakeStroke.Marshall()
+	packet := socket.RawMessage{
+		MessageType: byte(socket.MessageType.StrokeChunkServer),
+		Length:      uint16(len(payload)),
+		Bytes:       payload,
+	}
+	socket.SendRawMessageToSocketID(packet, socketID)
+}
+
+func (s *Stroke) Marshall() []byte {
+	if s.ID == uuid.Nil {
+		s.ID = uuid.New()
+	}
+	if s.isEraser {
+		s.color = 0x01
+		s.isSquared = false
+	}
+	var firstByte byte
+	firstByte = 0x00
+	firstByte |= s.color
+
+	if s.isSquared {
+		firstByte |= 1 << 6
+	}
+	if s.isEraser {
+		firstByte |= 1 << 7
+	}
+	strokeID, _ := s.ID.MarshalBinary()
+	nulID, _ := uuid.Nil.MarshalBinary()
+	width := make([]byte, 2)
+	height := make([]byte, 2)
+
+	binary.BigEndian.PutUint16(width, s.width)
+	binary.BigEndian.PutUint16(height, s.height)
+
+	response := make([]byte, 0, 40)
+	response = append(response, firstByte)
+	response = append(response, strokeID...)
+	response = append(response, nulID...)
+	response = append(response, width...)
+	response = append(response, height...)
+	response = append(response, s.brushSize)
+	for i := range s.points {
+		response = append(response, s.points[i].Marshall()...)
+	}
+	return response
+}
+
+func (p *point) Marshall() []byte {
+	//Round the number up if larger than uint16 max
+	p.round()
+	x := uint16(p.X)
+	y := uint16(p.Y)
+	xArray := make([]byte, 2)
+	yArray := make([]byte, 2)
+
+	binary.BigEndian.PutUint16(xArray, x)
+	binary.BigEndian.PutUint16(yArray, y)
+
+	response := make([]byte, 0, 4)
+
+	response = append(response, xArray...)
+	response = append(response, yArray...)
+
+	return response
+}
+
+func (p *point) round() {
+	if p.X > float32(MaxUint16) {
+		p.X = float32(MaxUint16)
+	}
+
+	if p.Y > float32(MaxUint16) {
+		p.Y = float32(MaxUint16)
+	}
 }
--- Server/internal/socket/message.go
+++ Server/internal/socket/message.go
@@ -26,6 +26,7 @@ type messageType struct {
 	UserCreateChannel        int
 	DestroyChannel           int
 	UserDestroyedChannel     int
+	StrokeChunkServer        int
 	StartDrawingServer       int
 	EndDrawingServer         int
 	PreviewDrawing           int
@@ -51,6 +52,7 @@ var MessageType = &messageType{
 	UserCreateChannel:        27,
 	DestroyChannel:           28,
 	UserDestroyedChannel:     29,
+	StrokeChunkServer:        31,
 	StartDrawingServer:       33,
 	EndDrawingServer:         35,
 	PreviewDrawing:           36,
