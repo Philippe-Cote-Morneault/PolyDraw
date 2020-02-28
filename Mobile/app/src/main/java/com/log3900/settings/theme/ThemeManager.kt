3c6cc39fc96b8cc97c3d8a84f33844afcd78d3f2
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-28 16:45:31 -0500

    ThemeManager now has arrayList instead of hashMap.

--- Mobile/app/src/main/java/com/log3900/settings/theme/ThemeManager.kt
+++ Mobile/app/src/main/java/com/log3900/settings/theme/ThemeManager.kt
@@ -9,58 +9,48 @@ import com.log3900.R
 
 class ThemeManager {
     companion object {
-        private val defaultThemeID = R.style.MyTheme_IndigoOrange
         private val registeredActivities: HashMap<Activity, Int> = HashMap()
-        var themes: HashMap<Int, Theme> = hashMapOf(
-            R.style.MyTheme_BlueYellow to Theme(R.style.MyTheme_BlueYellow, R.drawable.blue_yellow_theme_button),
-            R.style.MyTheme_BrownGreen to Theme(R.style.MyTheme_BrownGreen, R.drawable.brown_green_theme_button),
-            R.style.MyTheme_GreenTeal to Theme(R.style.MyTheme_GreenTeal, R.drawable.green_teal_theme_button),
-            R.style.MyTheme_IndigoOrange to Theme(R.style.MyTheme_IndigoOrange, R.drawable.indigo_orange_theme_button),
-            R.style.MyTheme_OrangeLightBlue to Theme(R.style.MyTheme_OrangeLightBlue, R.drawable.orange_light_blue_theme_button),
-            R.style.MyTheme_PurpleGreen to Theme(R.style.MyTheme_PurpleGreen, R.drawable.purple_green_theme_button),
-            R.style.MyTheme_RedYellow to Theme(R.style.MyTheme_RedYellow, R.drawable.red_yellow_theme_button),
-            R.style.MyTheme_TealLime to Theme(R.style.MyTheme_TealLime, R.drawable.teal_lime_theme_button)
+        var themes: ArrayList<Theme> = arrayListOf(
+            Theme(0, R.style.MyTheme_BlueYellow, R.drawable.blue_yellow_theme_button),
+            Theme(1, R.style.MyTheme_BrownGreen, R.drawable.brown_green_theme_button),
+            Theme(2, R.style.MyTheme_GreenTeal, R.drawable.green_teal_theme_button),
+            Theme(3, R.style.MyTheme_IndigoOrange, R.drawable.indigo_orange_theme_button),
+            Theme(4, R.style.MyTheme_OrangeLightBlue, R.drawable.orange_light_blue_theme_button),
+            Theme(5, R.style.MyTheme_PurpleGreen, R.drawable.purple_green_theme_button),
+            Theme(6, R.style.MyTheme_RedYellow, R.drawable.red_yellow_theme_button),
+            Theme(7, R.style.MyTheme_TealLime, R.drawable.teal_lime_theme_button)
         )
 
         fun applyTheme(activity: Activity) {
-            val context = MainApplication.instance.applicationContext
-            val preferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-            val theme = preferences.getInt(context.getString(R.string.preference_file_theme_key), defaultThemeID)
             activity.setTheme(
-                themes[theme]?.themeID!!
+                getCurrentTheme().themeID
             )
 
-            registeredActivities[activity] = theme
+            registeredActivities[activity] = getCurrentTheme().index
         }
 
         fun hasActivityThemeChanged(activity: Activity): Boolean {
-            return registeredActivities[activity] != getCurrentTheme().themeID
+            return registeredActivities[activity] != getCurrentTheme().index
         }
 
         fun getCurrentTheme(): Theme {
             val context = MainApplication.instance.applicationContext
             val preferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-            val themeID = preferences.getInt(context.getString(R.string.preference_file_theme_key), defaultThemeID)
-            return themes[themeID]!!
+            val themeIndex = preferences.getInt(context.getString(R.string.preference_file_theme_key), 3)
+            return themes[themeIndex]
         }
 
         fun changeTheme(theme: Theme) {
             val context = MainApplication.instance.applicationContext
             val preferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
             preferences.edit {
-                putInt(context.getString(R.string.preference_file_theme_key), theme.themeID)
+                putInt(context.getString(R.string.preference_file_theme_key), theme.index)
                 commit()
             }
         }
 
         fun getThemesAsArrayList(): ArrayList<Theme> {
-            val arrayList = arrayListOf<Theme>()
-
-            themes.forEach {
-                arrayList.add(it.value)
-            }
-
-            return arrayList
+            return themes
         }
     }
 }
