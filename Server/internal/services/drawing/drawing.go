81268c0a98d4e9a9ac178ffbec5db9a017b735da
Martin Pouliot <martinp507@gmail.com>
2020-02-27 22:09:55 -0500

    Made the basic drawing service

---
+++ Server/internal/services/drawing/drawing.go
@@ -0,0 +1,62 @@
+package drawing
+
+import (
+	"log"
+
+	service "gitlab.com/jigsawcorp/log3900/internal/services"
+	"gitlab.com/jigsawcorp/log3900/internal/socket"
+	"gitlab.com/jigsawcorp/log3900/pkg/cbroadcast"
+)
+
+//Drawing service used to route the packets and send the preview
+type Drawing struct {
+	connected cbroadcast.Channel
+	close     cbroadcast.Channel
+
+	shutdown chan bool
+}
+
+//Init the drawing service
+func (d *Drawing) Init() {
+	d.shutdown = make(chan bool)
+	d.subscribe()
+}
+
+//Start the drawing service
+func (d *Drawing) Start() {
+	log.Println("[Drawing] -> Starting service")
+	go d.listen()
+}
+
+//Shutdown the drawing service
+func (d *Drawing) Shutdown() {
+	log.Println("[Drawing] -> Closing service")
+	close(d.shutdown)
+}
+
+//Register register any broadcast not used
+func (d *Drawing) Register() {
+
+}
+
+func (d *Drawing) listen() {
+	defer service.Closed()
+
+	//Message viewer
+	for {
+		select {
+		case id := <-d.connected:
+			log.Printf("[Drawing] -> connected id: %s", id)
+		case id := <-d.close:
+			log.Printf("[Drawing] -> disconnect id: %s", id)
+		case <-d.shutdown:
+			return
+		}
+	}
+
+}
+
+func (d *Drawing) subscribe() {
+	d.connected, _, _ = cbroadcast.Subscribe(socket.BSocketConnected)
+	d.close, _, _ = cbroadcast.Subscribe(socket.BSocketCloseClient)
+}
