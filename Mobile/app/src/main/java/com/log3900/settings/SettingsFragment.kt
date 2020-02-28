4fc298732c555b09aad41d16dcfd03748cabbdc5
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-28 13:24:41 -0500

    Removed useless imports.

--- Mobile/app/src/main/java/com/log3900/settings/SettingsFragment.kt
+++ Mobile/app/src/main/java/com/log3900/settings/SettingsFragment.kt
@@ -1,15 +1,10 @@
 package com.log3900.settings
 
-import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.log3900.R
+import com.log3900.settings.theme.ThemePickerFragment
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
