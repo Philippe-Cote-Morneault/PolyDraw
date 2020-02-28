75fe25041e2cd93c73907f02265e4e0989bdc6ea
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-28 13:24:53 -0500

    Theme Manager now holds all themes.

--- Mobile/app/src/main/java/com/log3900/settings/theme/ThemeManager.kt
+++ Mobile/app/src/main/java/com/log3900/settings/theme/ThemeManager.kt
@@ -1,26 +1,60 @@
-package com.log3900.settings
+package com.log3900.settings.theme
 
 import android.app.Activity
 import android.content.Context
+import android.util.Log
+import androidx.core.content.edit
 import com.log3900.MainApplication
 import com.log3900.R
 
 class ThemeManager {
     companion object {
-        private val themeResourceIDs = intArrayOf(R.style.MyTheme_BlueYellow, R.style.MyTheme_OrangeBlue)
+        private val defaultThemeID = R.style.MyTheme_BlueYellow
+        private val registeredActivities: HashMap<Activity, Int> = HashMap()
+        var themes: HashMap<Int, Theme> = hashMapOf(
+            R.style.MyTheme_BlueYellow to Theme(R.style.MyTheme_BlueYellow, R.drawable.green_turtoise_theme_button),
+            R.style.MyTheme_OrangeBlue to Theme(R.style.MyTheme_OrangeBlue, R.drawable.blue_orang_theme_button)
+        )
+
         fun applyTheme(activity: Activity) {
             val context = MainApplication.instance.applicationContext
             val preferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-            val theme = preferences.getInt(context.getString(R.string.preference_file_theme_key), 0)
-            activity.setTheme(getThemeResourceFromIndex(theme))
+            val theme = preferences.getInt(context.getString(R.string.preference_file_theme_key), defaultThemeID)
+            activity.setTheme(
+                themes[theme]?.themeID!!
+            )
+
+            registeredActivities[activity] = theme
         }
 
-        fun changeTheme() {
-
+        fun hasActivityThemeChanged(activity: Activity): Boolean {
+            return registeredActivities[activity] != getCurrentTheme().themeID
         }
 
-        private fun getThemeResourceFromIndex(themeIndex: Int): Int {
-            return themeResourceIDs[themeIndex]
+        fun getCurrentTheme(): Theme {
+            val context = MainApplication.instance.applicationContext
+            val preferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            val themeID = preferences.getInt(context.getString(R.string.preference_file_theme_key), defaultThemeID)
+            return themes[themeID]!!
+        }
+
+        fun changeTheme(theme: Theme) {
+            val context = MainApplication.instance.applicationContext
+            val preferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            preferences.edit {
+                putInt(context.getString(R.string.preference_file_theme_key), theme.themeID)
+                commit()
+            }
+        }
+
+        fun getThemesAsArrayList(): ArrayList<Theme> {
+            val arrayList = arrayListOf<Theme>()
+
+            themes.forEach {
+                arrayList.add(it.value)
+            }
+
+            return arrayList
         }
     }
 }
