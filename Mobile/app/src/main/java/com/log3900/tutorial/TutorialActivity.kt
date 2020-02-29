9e3332a4cd7a9f74d8787c9cc220f65d5a210a15
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-29 00:10:07 -0500

    Added TutorialActivity.

---
+++ Mobile/app/src/main/java/com/log3900/tutorial/TutorialActivity.kt
@@ -0,0 +1,10 @@
+package com.log3900.tutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
