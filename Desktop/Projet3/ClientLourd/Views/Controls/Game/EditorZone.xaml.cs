ba5daa0f97981c3ddb77cc2daa890bb783292a0f
cedric <cedric.tessier@polymtl.ca>
2020-02-29 16:25:38 -0500

    Created all the user control for the game view

---
+++ Desktop/Projet3/ClientLourd/Views/Controls/Game/EditorZone.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ClientLourd.Views.Controls.Game
+{
+    public partial class EditorZone : UserControl
+    {
+        public EditorZone()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Desktop/Projet3/ClientLourd/Views/Controls/Game/TopBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ClientLourd.Views.Controls.Game
+{
+    public partial class TopBar : UserControl
+    {
+        public TopBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
