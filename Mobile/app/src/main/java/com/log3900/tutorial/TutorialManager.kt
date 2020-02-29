f7cf192643b7949cf1db789ef30aec330b5e8a92
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-29 17:54:56 -0500

    TutorialManager now has a mapping of tutorial slides and titles.

--- Mobile/app/src/main/java/com/log3900/tutorial/TutorialManager.kt
+++ Mobile/app/src/main/java/com/log3900/tutorial/TutorialManager.kt
@@ -1,12 +1,23 @@
 package com.log3900.tutorial
 
+import com.log3900.R
 import com.log3900.tutorial.slides.ChatFragment01
 import com.log3900.tutorial.slides.WelcomeFragment
 
 class TutorialManager {
-    var tutorialSlides: ArrayList<Class<*>> = arrayListOf(
-        WelcomeFragment::class.java,
-        ChatFragment01::class.java
+    var tutorialSlides: HashMap<Int, Class<*>> = hashMapOf(
+        R.string.tutorial_slide_welcome_01_title to WelcomeFragment::class.java,
+        R.string.tutorial_slide_chat_01_title to ChatFragment01::class.java
     )
+
+    fun getSlidesAsArrayList(): ArrayList<Class<*>> {
+        var arrayList = arrayListOf<Class<*>>()
+
+        tutorialSlides.forEach {
+            arrayList.add(it.value)
+        }
+
+        return arrayList
+    }
 
 }
