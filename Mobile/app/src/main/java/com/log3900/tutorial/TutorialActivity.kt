c968af14f260268eb08b662240714cf7533048d7
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-29 12:53:49 -0500

    TutorialActivity can now move through viewPager with buttons.

--- Mobile/app/src/main/java/com/log3900/tutorial/TutorialActivity.kt
+++ Mobile/app/src/main/java/com/log3900/tutorial/TutorialActivity.kt
@@ -5,16 +5,22 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.button.MaterialButton
 import com.log3900.R
 import com.log3900.settings.theme.ThemeManager
 
 class TutorialActivity : AppCompatActivity() {
+    // UI Elements
     private lateinit var toolbar: Toolbar
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var previousButton: MaterialButton
     private lateinit var skipTutorialButton: MaterialButton
     private lateinit var nextButton: MaterialButton
+    private lateinit var viewPager: ViewPager
+
+    private lateinit var tutorialSlidesAdapter: TutorialFragmentPagerAdapter
+    private var tutorialManager: TutorialManager = TutorialManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         ThemeManager.applyTheme(this)
@@ -30,6 +36,9 @@ class TutorialActivity : AppCompatActivity() {
         previousButton = findViewById(R.id.activity_tutorial_footer_button_prev)
         skipTutorialButton = findViewById(R.id.activity_tutorial_footer_button_skip)
         nextButton = findViewById(R.id.activity_tutorial_footer_button_next)
+        viewPager = findViewById(R.id.activity_tutorial_view_pager)
+
+        setupViewPager()
 
         addButtonListeners()
 
@@ -43,12 +52,19 @@ class TutorialActivity : AppCompatActivity() {
         nextButton.setOnClickListener { onNextButtonClick() }
     }
 
+    private fun setupViewPager() {
+        tutorialSlidesAdapter = TutorialFragmentPagerAdapter(supportFragmentManager, tutorialManager.tutorialSlides)
+        viewPager.adapter = tutorialSlidesAdapter
+    }
+
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
     }
 
     private fun onPreviousButtonClick() {
-
+        if (viewPager.currentItem > 0) {
+            viewPager.setCurrentItem(viewPager.currentItem - 1)
+        }
     }
 
     private fun onSkipTutorialButtonClick() {
@@ -56,6 +72,8 @@ class TutorialActivity : AppCompatActivity() {
     }
 
     private fun onNextButtonClick() {
-
+        if (viewPager.currentItem < tutorialSlidesAdapter.count - 1) {
+            viewPager.setCurrentItem(viewPager.currentItem + 1)
+        }
     }
 }
