22dad60bbda233971f3f8cf3946b5a9b0a9afae8
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-01 14:12:29 -0500

    Added UnreadMessages map to ChannelGroup.

--- Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelGroup.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Channel/ChannelGroup.kt
@@ -1,10 +1,15 @@
 package com.log3900.chat.Channel
 
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
 enum class GroupType {
     AVAILABLE,
     JOINED
 }
-class ChannelGroup(var type: GroupType, var channels: ArrayList<Channel>, var filteredChannels: ArrayList<Channel> = arrayListOf()) {
+class ChannelGroup(var type: GroupType, var channels: ArrayList<Channel>, var unreadMessages: HashMap<UUID, Int>,
+                   var filteredChannels: ArrayList<Channel> = arrayListOf(), var activeChannel: Channel? = null) {
     fun getName(): String {
         when (type) {
             GroupType.AVAILABLE -> return "Available"
