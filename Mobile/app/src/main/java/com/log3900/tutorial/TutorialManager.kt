61b88ad8c167440ffbe28b4358f6795b8dc87405
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-29 19:59:40 -0500

    TutorialManger is now a Singleton.

--- Mobile/app/src/main/java/com/log3900/tutorial/TutorialManager.kt
+++ Mobile/app/src/main/java/com/log3900/tutorial/TutorialManager.kt
@@ -4,11 +4,31 @@ import com.log3900.R
 import com.log3900.tutorial.slides.ChatFragment01
 import com.log3900.tutorial.slides.WelcomeFragment
 
-class TutorialManager {
+object TutorialManager {
+    private var activeTutorialTab: Int = 0
+
+    private var slideChangedListeners: ArrayList<((oldPos: Int, newPos: Int) -> Unit)> = arrayListOf()
+
     var tutorialSlides: HashMap<Int, Class<*>> = hashMapOf(
         R.string.tutorial_slide_welcome_01_title to WelcomeFragment::class.java,
         R.string.tutorial_slide_chat_01_title to ChatFragment01::class.java
     )
+
+    fun addSlideChangedListener(listener: ((oldPos: Int, newPos: Int) -> Unit)) {
+        slideChangedListeners.add(listener)
+    }
+
+    fun changeActiveTutorialSlide(position: Int) {
+        slideChangedListeners.forEach {
+            it(activeTutorialTab, position)
+        }
+
+        activeTutorialTab = position
+    }
+
+    fun getActiveTutorialSlide(): Int {
+        return activeTutorialTab
+    }
 
     fun getSlidesAsArrayList(): ArrayList<Class<*>> {
         var arrayList = arrayListOf<Class<*>>()
@@ -20,4 +40,13 @@ class TutorialManager {
         return arrayList
     }
 
+    fun getSlidesTitlesAsArrayList(): ArrayList<Int> {
+        val arrayList = arrayListOf<Int>()
+
+        tutorialSlides.forEach {
+            arrayList.add(it.key)
+        }
+
+        return arrayList
+    }
 }
