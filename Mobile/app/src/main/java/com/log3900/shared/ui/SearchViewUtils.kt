76e1a116fe21482b1dcb17abafad2c963332696a
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-01 15:41:25 -0500

    Created SearchView utils to see if SearchView is focused.

---
+++ Mobile/app/src/main/java/com/log3900/shared/ui/SearchViewUtils.kt
@@ -0,0 +1,26 @@
+package com.log3900.shared.ui
+
+import android.widget.SearchView
+import android.R
+import android.view.View
+import android.view.ViewGroup
+
+
+
+
+
+object SearchViewUtils {
+    fun isFocused(view: View): Boolean {
+        if (view.isFocused())
+            return true
+
+        if (view is ViewGroup) {
+            val viewGroup = view
+            for (i in 0 until viewGroup.childCount) {
+                if (isFocused(viewGroup.getChildAt(i)))
+                    return true
+            }
+        }
+        return false
+    }
+}
