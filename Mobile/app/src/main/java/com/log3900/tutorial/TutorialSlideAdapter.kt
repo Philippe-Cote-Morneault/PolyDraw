fd845d4843b7a282071d411d515f48f93ae896bb
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-29 20:00:22 -0500

    Added event listeners on TutorialSlideAdapter.

--- Mobile/app/src/main/java/com/log3900/tutorial/TutorialSlideAdapter.kt
+++ Mobile/app/src/main/java/com/log3900/tutorial/TutorialSlideAdapter.kt
@@ -2,8 +2,10 @@ package com.log3900.tutorial
 
 import android.content.res.Resources
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.log3900.MainApplication
 import com.log3900.R
 
 class TutorialSlideAdapter(var tutorialSlides: ArrayList<Int>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -12,7 +14,13 @@ class TutorialSlideAdapter(var tutorialSlides: ArrayList<Int>) : RecyclerView.Ad
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_tutorial_slide, parent, false)
 
-        return TutorialSlideViewHolder(view)
+        return TutorialSlideViewHolder(view, object: TutorialSlideViewHolder.ClickListener {
+            override fun onSlideClick(slideIndex: Int) {
+                if (slideIndex != TutorialManager.getActiveTutorialSlide()) {
+                    TutorialManager.changeActiveTutorialSlide(slideIndex)
+                }
+            }
+        })
     }
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
@@ -22,7 +30,11 @@ class TutorialSlideAdapter(var tutorialSlides: ArrayList<Int>) : RecyclerView.Ad
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as TutorialSlideViewHolder).bind(Resources.getSystem().getString(tutorialSlides[position]))
+        (holder as TutorialSlideViewHolder).bind(
+            MainApplication.instance.getString(tutorialSlides[position]),
+            position,
+            TutorialManager.getActiveTutorialSlide() == position
+        )
     }
 
     override fun getItemCount(): Int {
