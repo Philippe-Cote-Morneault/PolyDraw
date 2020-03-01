df1d891dd4607923ccf452d669dae883b6e6f50d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-02-29 20:01:09 -0500

    Added RecyclerView to TutorialSlideListFragment.

--- Mobile/app/src/main/java/com/log3900/tutorial/TutorialSlidesListFragment.kt
+++ Mobile/app/src/main/java/com/log3900/tutorial/TutorialSlidesListFragment.kt
@@ -5,11 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.log3900.R
+import com.log3900.settings.theme.ThemeAdapter
 
 class TutorialSlidesListFragment : Fragment() {
+    private lateinit var tutorialSlidesRecyclerView: RecyclerView
+    private lateinit var tutorialSlidesAdapter: TutorialSlideAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_tutorial_slides_list, container, false)
+
+        tutorialSlidesRecyclerView = root.findViewById(R.id.fragment_tutorial_slides_list_recycler_view)
+
+        setupRecyclerView()
+
+        TutorialManager.addSlideChangedListener { oldPos, newPos ->
+            onSlideChanged(oldPos, newPos)
+        }
+
         return root
+    }
+
+    private fun onSlideChanged(oldPos: Int, newPos: Int) {
+        tutorialSlidesAdapter.notifyItemChanged(oldPos)
+        tutorialSlidesAdapter.notifyItemChanged(newPos)
+    }
+
+    private fun setupRecyclerView() {
+        tutorialSlidesAdapter = TutorialSlideAdapter(TutorialManager.getSlidesTitlesAsArrayList())
+        tutorialSlidesRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = tutorialSlidesAdapter
+        }
     }
 }
