cea70986f85dd722f06f1e36b7f2e3ef24335f49
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-02 11:15:26 -0500

    MessageManager will unsubscribe from MessageRepository when it is deleted.

--- Mobile/app/src/main/java/com/log3900/chat/Message/MessageManager.kt
+++ Mobile/app/src/main/java/com/log3900/chat/Message/MessageManager.kt
@@ -1,6 +1,7 @@
 package com.log3900.chat.Message
 
 import android.os.Handler
+import android.util.Log
 import com.log3900.chat.Channel.Channel
 import com.log3900.chat.ChatMessage
 import com.log3900.shared.architecture.EventType
@@ -11,30 +12,35 @@ import org.greenrobot.eventbus.EventBus
 import java.util.*
 
 class MessageManager {
-    private var messageRepository: MessageRepository
-    private var chatMessages: LinkedList<ChatMessage> = LinkedList()
+    private var messageRepository: MessageRepository? = null
+    private lateinit var socketMessageHandler: Handler
 
     constructor() {
         messageRepository = MessageRepository.instance!!
     }
 
     fun init() {
-        messageRepository.subscribe(MessageRepository.Event.CHAT_MESSAGE_RECEIVED, Handler {
+        socketMessageHandler = messageRepository!!.subscribe(MessageRepository.Event.CHAT_MESSAGE_RECEIVED,Handler{
             onMessageReceived(it.obj as ChatMessage)
             true
         })
     }
 
+    fun delete() {
+        messageRepository?.unsubscribe(MessageRepository.Event.CHAT_MESSAGE_RECEIVED, socketMessageHandler)
+        messageRepository = null
+    }
+
     fun getMessages(channel: Channel): Single<LinkedList<ChatMessage>> {
-        return messageRepository.getChannelMessages(channel.ID)
+        return messageRepository!!.getChannelMessages(channel.ID)
     }
 
     fun sendMessage(channelID:UUID, message: String) {
-        messageRepository.sendMessage(SentMessage(message, channelID))
+        messageRepository?.sendMessage(SentMessage(message, channelID))
     }
 
     fun loadMoreMessages(channelID: UUID): Single<Int> {
-        return messageRepository.loadMoreMessages(15, channelID)
+        return messageRepository!!.loadMoreMessages(15, channelID)
     }
 
     private fun onMessageReceived(message: ChatMessage) {
