26607ba6cc4e9302fa8f0a3ce226c310f04c913d
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-03-02 19:08:27 -0500

    Formatting fix

--- Mobile/app/src/main/java/com/log3900/draw/DrawViewBase.kt
+++ Mobile/app/src/main/java/com/log3900/draw/DrawViewBase.kt
@@ -113,119 +113,119 @@ open class DrawViewBase @JvmOverloads constructor(
         mPaintOptions = options
     }
 
-        fun setColor(newColor: Int) {
-            @ColorInt
-            val alphaColor = ColorUtils.setAlphaComponent(newColor, mPaintOptions.alpha)
-            mPaintOptions.color = alphaColor
-            if (mIsStrokeWidthBarEnabled) {
-                invalidate()
-            }
-        }
-
-        fun setStrokeWidth(newStrokeWidth: Float) {
-            mPaintOptions.strokeWidth = newStrokeWidth
-            if (mIsStrokeWidthBarEnabled) {
-                invalidate()
-            }
-        }
-
-        fun getBitmap(): Bitmap {
-            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-            val canvas = Canvas(bitmap)
-            canvas.drawColor(Color.WHITE)
-            mIsSaving = true
-            draw(canvas)
-            mIsSaving = false
-            return bitmap
-        }
-
-        fun addPath(path: MyPath, options: PaintOptions) {
-            mPaths[path] = options
-        }
-
-        override fun onDraw(canvas: Canvas) {
-            super.onDraw(canvas)
-
-            for ((key, value) in mPaths) {
-                changePaint(value)
-                canvas.drawPath(key, mPaint)
-            }
-
-            changePaint(mPaintOptions)
-            canvas.drawPath(mPath, mPaint)
-        }
-
-        private fun changePaint(paintOptions: PaintOptions) {
-            mPaint.color = if (paintOptions.drawMode == DrawMode.ERASE) Color.WHITE else paintOptions.color
-            mPaint.strokeWidth = paintOptions.strokeWidth
-            mPaint.strokeCap = paintOptions.strokeCap
-        }
-
-        fun clearCanvas() {
-            mLastPaths = mPaths.clone() as LinkedHashMap<MyPath, PaintOptions>
-            mPath.reset()
-            mPaths.clear()
+    fun setColor(newColor: Int) {
+        @ColorInt
+        val alphaColor = ColorUtils.setAlphaComponent(newColor, mPaintOptions.alpha)
+        mPaintOptions.color = alphaColor
+        if (mIsStrokeWidthBarEnabled) {
             invalidate()
         }
+    }
 
-        override fun onTouchEvent(event: MotionEvent): Boolean {
+    fun setStrokeWidth(newStrokeWidth: Float) {
+        mPaintOptions.strokeWidth = newStrokeWidth
+        if (mIsStrokeWidthBarEnabled) {
+            invalidate()
+        }
+    }
+
+    fun getBitmap(): Bitmap {
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.WHITE)
+        mIsSaving = true
+        draw(canvas)
+        mIsSaving = false
+        return bitmap
+    }
+
+    fun addPath(path: MyPath, options: PaintOptions) {
+        mPaths[path] = options
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        for ((key, value) in mPaths) {
+            changePaint(value)
+            canvas.drawPath(key, mPaint)
+        }
+
+        changePaint(mPaintOptions)
+        canvas.drawPath(mPath, mPaint)
+    }
+
+    private fun changePaint(paintOptions: PaintOptions) {
+        mPaint.color = if (paintOptions.drawMode == DrawMode.ERASE) Color.WHITE else paintOptions.color
+        mPaint.strokeWidth = paintOptions.strokeWidth
+        mPaint.strokeCap = paintOptions.strokeCap
+    }
+
+    fun clearCanvas() {
+        mLastPaths = mPaths.clone() as LinkedHashMap<MyPath, PaintOptions>
+        mPath.reset()
+        mPaths.clear()
+        invalidate()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
 //        return true
 
-            // TODO: REmove when done testing
-            val x = event.x
-            val y = event.y
+        // TODO: REmove when done testing
+        val x = event.x
+        val y = event.y
 
-            if (mPaintOptions.drawMode == DrawMode.REMOVE) {
+        if (mPaintOptions.drawMode == DrawMode.REMOVE) {
 //            if (event.action == MotionEvent.ACTION_DOWN)
-                removePathIfIntersection(x, y)
-                return true
-            }
-
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    mStartX = x
-                    mStartY = y
-                    drawStart(DrawPoint(x, y))
-                }
-                MotionEvent.ACTION_MOVE -> drawMove(DrawPoint(x, y))
-                MotionEvent.ACTION_UP -> drawEnd()
-            }
-
-            invalidate()
+            removePathIfIntersection(x, y)
             return true
         }
 
-        private fun removePathIfIntersection(x: Float, y: Float) {
-            val sortedMap = mPaths
-            var keyToRemove = MyPath()
-            var found = false
-            for ((key, value) in sortedMap) {
-                for (action in key.actions) {
-                    var width = 30
-                    if (value.strokeWidth > 30)
-                        width = value.strokeWidth.toInt()
-                    if (action is Quad) {
-                        val q: Quad = action
-                        val distance1 = sqrt((q.x1.toDouble() - x.toDouble()).pow(2.0) + (q.y1.toDouble() - y.toDouble()).pow(2.0))
-                        val distance2 = sqrt((q.x2.toDouble() - x.toDouble()).pow(2.0) + (q.y2.toDouble() - y.toDouble()).pow(2.0))
-                        if (value.color != 0xFFFFFFFF.toInt() && (distance1 <= width || distance2 <= width)) {
-                            found = true
-                            keyToRemove = key
-                        }
-                    } else if (action is Line) {
-                        val q: Line = action
-                        val distance = sqrt((q.x.toDouble() - x.toDouble()).pow(2.0) + (q.y.toDouble() - y.toDouble()).pow(2.0))
-                        if (distance <= width && value.color != 0xFFFFFFFF.toInt()) {
-                            found = true
-                            keyToRemove = key
-                        }
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mStartX = x
+                mStartY = y
+                drawStart(DrawPoint(x, y))
+            }
+            MotionEvent.ACTION_MOVE -> drawMove(DrawPoint(x, y))
+            MotionEvent.ACTION_UP -> drawEnd()
+        }
+
+        invalidate()
+        return true
+    }
+
+    private fun removePathIfIntersection(x: Float, y: Float) {
+        val sortedMap = mPaths
+        var keyToRemove = MyPath()
+        var found = false
+        for ((key, value) in sortedMap) {
+            for (action in key.actions) {
+                var width = 30
+                if (value.strokeWidth > 30)
+                    width = value.strokeWidth.toInt()
+                if (action is Quad) {
+                    val q: Quad = action
+                    val distance1 = sqrt((q.x1.toDouble() - x.toDouble()).pow(2.0) + (q.y1.toDouble() - y.toDouble()).pow(2.0))
+                    val distance2 = sqrt((q.x2.toDouble() - x.toDouble()).pow(2.0) + (q.y2.toDouble() - y.toDouble()).pow(2.0))
+                    if (value.color != 0xFFFFFFFF.toInt() && (distance1 <= width || distance2 <= width)) {
+                        found = true
+                        keyToRemove = key
+                    }
+                } else if (action is Line) {
+                    val q: Line = action
+                    val distance = sqrt((q.x.toDouble() - x.toDouble()).pow(2.0) + (q.y.toDouble() - y.toDouble()).pow(2.0))
+                    if (distance <= width && value.color != 0xFFFFFFFF.toInt()) {
+                        found = true
+                        keyToRemove = key
                     }
                 }
             }
-            if(found) {
-                mPaths.remove(keyToRemove)
-                invalidate()
-            }
+        }
+        if(found) {
+            mPaths.remove(keyToRemove)
+            invalidate()
+        }
 //        val point = FloatPoint(x, y)
 //        println("Checking point: $point")
 //        mPaths.forEach { (myPath, paintOptions) ->
@@ -236,21 +236,20 @@ open class DrawViewBase @JvmOverloads constructor(
 //                mPaths.remove(myPath)
 //            }
 //        }
-            invalidate()
-        }
+        invalidate()
+    }
 
-        fun setCap(cap: Paint.Cap) {
-            mPaintOptions.strokeCap = cap
-        }
+    fun setCap(cap: Paint.Cap) {
+        mPaintOptions.strokeCap = cap
+    }
 
-        fun setDrawMode(mode: DrawMode) {
-            mPaintOptions.drawMode = mode
-            when (mode) {
-                DrawMode.DRAW -> {}
-                DrawMode.REMOVE -> {}
-                DrawMode.ERASE -> {
-                    invalidate()
-                }
+    fun setDrawMode(mode: DrawMode) {
+        mPaintOptions.drawMode = mode
+        when (mode) {
+            DrawMode.DRAW -> {}
+            DrawMode.REMOVE -> {}
+            DrawMode.ERASE -> {
+                invalidate()
             }
         }
     }
