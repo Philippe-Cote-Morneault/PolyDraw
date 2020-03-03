ac927fc825e11cc939b39d336a005e4c88c3e295
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-03 16:45:19 -0500

    Added some fields to Group.

--- Mobile/app/src/main/java/com/log3900/game/group/Group.kt
+++ Mobile/app/src/main/java/com/log3900/game/group/Group.kt
@@ -4,9 +4,10 @@ import com.squareup.moshi.Json
 import java.util.*
 import kotlin.collections.ArrayList
 
-class Group(@Json(name = "GroupID") var groupID: UUID, @Json(name = "GroupName") var groupName: String,
-            @Json(name = "GameType") var gameType: Int, @Json(name = "PlayersInGroup") var playersInGroup: Int,
+class Group(@Json(name = "ID") var ID: UUID, @Json(name = "GroupName") var groupName: String,
             @Json(name = "PlayersMax") var playersMax: Int, @Json(name = "VirtualPlayer") var virtualPlayers: Int,
+            @Json(name = "GameType") var gameType: Int, @Json(name = "Difficulty") var difficulty: Int,
+            @Json(name = "Status") var status: Int, @Json(name = "Owner") var owner: UUID,
             @Json(name = "Players") var players: ArrayList<UUID>) {
 
 }
