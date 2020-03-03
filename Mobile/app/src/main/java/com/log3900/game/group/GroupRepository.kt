db7aa4aedaaef02ec00590e1d8cc34a2cf88ce07
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-03 14:34:04 -0500

    GroupRepository can now fetch all groups from REST.

--- Mobile/app/src/main/java/com/log3900/game/group/GroupRepository.kt
+++ Mobile/app/src/main/java/com/log3900/game/group/GroupRepository.kt
@@ -6,7 +6,18 @@ import android.os.Binder
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import com.google.gson.JsonArray
 import com.log3900.socket.SocketService
+import com.log3900.user.account.AccountRepository
+import com.log3900.utils.format.moshi.UUIDAdapter
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.Single
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class GroupRepository : Service() {
     private val binder = GroupRepositoryBinder()
@@ -20,13 +31,54 @@ class GroupRepository : Service() {
         var instance: GroupRepository? = null
     }
 
+    private fun initializeRepository() {
+        instance = this
+        socketService = SocketService.instance
+        groupCache = GroupCache()
+        getGroups(AccountRepository.getInstance().getAccount().sessionToken).subscribe(
+            {
+                println("Groups = ")
+                it.forEach {group ->
+                    println(group.toString())
+                }
+                isReady = true
+            },
+            {
+
+            }
+        )
+    }
+
+    fun getGroups(sessionToken: String): Single<ArrayList<Group>> {
+        return Single.create {
+            val call = GroupRestService.service.getGroups(sessionToken, "EN")
+            call.enqueue(object : Callback<JsonArray> {
+                override fun onResponse(call: Call<JsonArray>, response: Response<JsonArray>) {
+                    val moshi = Moshi.Builder()
+                        .add(KotlinJsonAdapterFactory())
+                        .add(UUIDAdapter())
+                        .build()
+
+                    val adapter: JsonAdapter<List<Group>> = moshi.adapter(Types.newParameterizedType(List::class.java, Group::class.java))
+                    val res = adapter.fromJson(response.body().toString())
+                    it.onSuccess(res as ArrayList<Group>)
+                }
+
+                override fun onFailure(call: Call<JsonArray>, t: Throwable) {
+                    println("onFailure")
+                }
+            })
+        }
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         return binder
     }
 
     override fun onCreate() {
         super.onCreate()
-        
+
+        initializeRepository()
 
         Thread(Runnable {
             Looper.prepare()
