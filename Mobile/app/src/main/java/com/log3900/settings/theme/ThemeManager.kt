c8eb101bf1b0463566e497552d7e9501917e74c3
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-02 19:50:21 -0500

    ThemeManager now assigns string resources to all themes.

--- Mobile/app/src/main/java/com/log3900/settings/theme/ThemeManager.kt
+++ Mobile/app/src/main/java/com/log3900/settings/theme/ThemeManager.kt
@@ -13,14 +13,14 @@ class ThemeManager {
     companion object {
         private val registeredActivities: HashMap<Activity, Int> = HashMap()
         var themes: ArrayList<Theme> = arrayListOf(
-            Theme(0, R.style.MyTheme_BlueYellow, R.drawable.blue_yellow_theme_button),
-            Theme(1, R.style.MyTheme_BrownGreen, R.drawable.brown_green_theme_button),
-            Theme(2, R.style.MyTheme_GreenTeal, R.drawable.green_teal_theme_button),
-            Theme(3, R.style.MyTheme_IndigoOrange, R.drawable.indigo_orange_theme_button),
-            Theme(4, R.style.MyTheme_OrangeLightBlue, R.drawable.orange_light_blue_theme_button),
-            Theme(5, R.style.MyTheme_PurpleGreen, R.drawable.purple_green_theme_button),
-            Theme(6, R.style.MyTheme_RedYellow, R.drawable.red_yellow_theme_button),
-            Theme(7, R.style.MyTheme_TealLime, R.drawable.teal_lime_theme_button)
+            Theme(0, R.style.MyTheme_BlueYellow, R.drawable.blue_yellow_theme_button, R.string.theme_blue_yellow_title),
+            Theme(1, R.style.MyTheme_BrownGreen, R.drawable.brown_green_theme_button, R.string.theme_brown_green_title),
+            Theme(2, R.style.MyTheme_GreenTeal, R.drawable.green_teal_theme_button, R.string.theme_green_teal_title),
+            Theme(3, R.style.MyTheme_IndigoOrange, R.drawable.indigo_orange_theme_button, R.string.theme_indigo_orange_title),
+            Theme(4, R.style.MyTheme_OrangeLightBlue, R.drawable.orange_light_blue_theme_button, R.string.theme_orange_light_blue_title),
+            Theme(5, R.style.MyTheme_PurpleGreen, R.drawable.purple_green_theme_button, R.string.theme_purple_green_title),
+            Theme(6, R.style.MyTheme_RedYellow, R.drawable.red_yellow_theme_button, R.string.theme_red_yellow_title),
+            Theme(7, R.style.MyTheme_TealLime, R.drawable.teal_lime_theme_button, R.string.theme_teal_lime_title)
         )
 
         fun applyTheme(activity: Activity) {
