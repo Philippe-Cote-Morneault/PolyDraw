9cd5ce9176cb6e57c13c72cf16a3e421ae96ae0d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 11:34:21 -0500

    Added MatchWaitingRoomView interface.

--- Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.log3900.R
 
-class MatchWaitingRoomFragment : Fragment() {
+class MatchWaitingRoomFragment : Fragment(), MatchWaitingRoomView {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_match_waiting_room, container, false)
 
