d1ea5e233071bc1cc4512ed8c11f6c9743c31faf
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-03 19:35:30 -0500

    MatchViewHolder now sets text properly based on current match.

--- Mobile/app/src/main/java/com/log3900/game/lobby/MatchViewHolder.kt
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchViewHolder.kt
@@ -5,9 +5,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.log3900.R
+import com.log3900.game.group.Group
 
 class MatchViewHolder : RecyclerView.ViewHolder {
-    
+    private lateinit var match: Group
+
     // UI
     private var matchIconImageView: ImageView
     private var matchModeTextView: TextView
@@ -23,7 +25,11 @@ class MatchViewHolder : RecyclerView.ViewHolder {
         matchPlayerCountTextView = itemView.findViewById(R.id.list_item_match_text_view_match_player_count)
     }
 
-    fun bind() {
-
+    fun bind(match: Group) {
+        this.match = match
+        matchModeTextView.text = match.gameType.toString()
+        matchNameTextView.text = match.groupName
+        matchHostTextView.text = match.owner.toString()
+        matchPlayerCountTextView.text = "${match.players.size}/${match.playersMax}"
     }
 }
