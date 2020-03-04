ffd6c1cbea3cb99d937c6c7ed075538c79b82497
Martin Pouliot <martinp507@gmail.com>
2020-03-04 13:32:00 -0500

    Added a struct for the players instead of socketID

--- Server/internal/services/match/mode/common.go
+++ Server/internal/services/match/mode/common.go
@@ -2,34 +2,59 @@ package mode
 
 import (
 	"github.com/google/uuid"
+	"gitlab.com/jigsawcorp/log3900/internal/services/auth"
 	"gitlab.com/jigsawcorp/log3900/internal/socket"
 	"gitlab.com/jigsawcorp/log3900/model"
 	"sync"
 )
 
+type players struct {
+	socketID uuid.UUID
+	userID   uuid.UUID
+	Username string
+}
+
 type base struct {
 	readyMatch  sync.WaitGroup
 	readyOnce   map[uuid.UUID]bool
-	connections []uuid.UUID
+	connections []players
 	info        model.Group
 }
 
 func (b *base) init(connections []uuid.UUID, info model.Group) {
-	b.connections = make([]uuid.UUID, len(connections))
-	copy(b.connections, connections)
+	b.connections = make([]players, len(connections))
+	for i := range connections {
+		socketID := connections[i]
+		userID, _ := auth.GetUserID(socketID)
+		//Find the user data in the game info
+		var user *model.User
+		for j := range info.Users {
+			if info.Users[j].ID == userID {
+				user = info.Users[j]
+			}
+		}
+		if user != nil && userID != uuid.Nil {
+			b.connections[i] = players{
+				socketID: socketID,
+				userID:   userID,
+				Username: user.Username,
+			}
+		}
+	}
+
 	b.info = info
 	b.readyMatch.Add(len(b.connections))
 
 	b.readyOnce = make(map[uuid.UUID]bool)
 	for i := range b.connections {
-		b.readyOnce[b.connections[i]] = false
+		b.readyOnce[b.connections[i].socketID] = false
 	}
 
 }
 
 func (b *base) broadcast(message *socket.RawMessage) {
 	for i := range b.connections {
-		socket.SendRawMessageToSocketID(*message, b.connections[i])
+		socket.SendRawMessageToSocketID(*message, b.connections[i].socketID)
 	}
 }
 
