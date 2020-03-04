c37498f78b4ec610e74f4bdd461ff246981b38c4
Martin Pouliot <martinp507@gmail.com>
2020-03-04 02:33:38 -0500

    Added call lobby start

--- Server/internal/services/lobby/broadcast.go
+++ Server/internal/services/lobby/broadcast.go
@@ -8,6 +8,8 @@ const BJoinGroup = "lobby:join"
 //BLeaveGroup leave a group
 const BLeaveGroup = "lobby:leave"
 
+const BStartMatch = "lobby:start"
+
 //BSize buffer size for the messenger service
 const BSize = 5
 
@@ -15,4 +17,5 @@ const BSize = 5
 func (l *Lobby) Register() {
 	cbroadcast.Register(BJoinGroup, BSize)
 	cbroadcast.Register(BLeaveGroup, BSize)
+	cbroadcast.Register(BStartMatch, BSize)
 }
