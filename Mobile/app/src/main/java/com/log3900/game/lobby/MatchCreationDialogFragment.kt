a44f1b3b5d13363c554d45b9493c7f29e35bcff6
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 09:45:36 -0500

    Added MatchCreationDialogFragment.

---
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchCreationDialogFragment.kt
@@ -0,0 +1,28 @@
+package com.log3900.game.lobby
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.log3900.R
+
+class MatchCreationDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialogBuilder = AlertDialog.Builder(activity)
+            .setTitle("Create Match")
+            .setPositiveButton("Create") { _, _ ->
+
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+
+            }
+
+        val view = activity?.layoutInflater?.inflate(R.layout.dialog_fragment_create_match, null)
+
+        dialogBuilder.setView(view)
+        return dialogBuilder.create()
+    }
+}
