ba762a732a748ca3c60eda9f8ecf472c39e604c2
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 12:30:44 -0500

    MatchCreationDialogFragment now takes click listeners as contsructor arguments.

--- Mobile/app/src/main/java/com/log3900/game/lobby/MatchCreationDialogFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchCreationDialogFragment.kt
@@ -2,6 +2,7 @@ package com.log3900.game.lobby
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,16 +11,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.log3900.R
 
-class MatchCreationDialogFragment : DialogFragment() {
+class MatchCreationDialogFragment(var positiveClickListener: DialogInterface.OnClickListener? = null,
+                                  var negativeClickListener: DialogInterface.OnClickListener? = null) : DialogFragment() {
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialogBuilder = AlertDialog.Builder(activity)
             .setTitle(resources.getString(R.string.create_match_dialog_title))
-            .setPositiveButton("Create") { _, _ ->
-
-            }
-            .setNegativeButton("Cancel") { _, _ ->
-
-            }
+            .setPositiveButton("Create", positiveClickListener)
+            .setNegativeButton("Cancel", negativeClickListener)
 
         val view = activity?.layoutInflater?.inflate(R.layout.dialog_fragment_create_match, null)
 
