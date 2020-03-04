cefb99cf846c550892c9a606edbe657fc5755ca9
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 16:59:35 -0500

    Fixed GroupAdapter to take into account enums.

--- Mobile/app/src/main/java/com/log3900/utils/format/moshi/GroupAdapter.kt
+++ Mobile/app/src/main/java/com/log3900/utils/format/moshi/GroupAdapter.kt
@@ -3,7 +3,9 @@ package com.log3900.utils.format.moshi
 import android.util.Log
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.log3900.game.group.Difficulty
 import com.log3900.game.group.Group
+import com.log3900.game.group.MatchMode
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import java.util.*
@@ -17,8 +19,8 @@ class GroupAdapter {
             groupJson.getAsJsonPrimitive("GroupName")!!.asString,
             groupJson.getAsJsonPrimitive("PlayersMax")!!.asInt,
             groupJson.getAsJsonPrimitive("VirtualPlayers")!!.asInt,
-            groupJson.getAsJsonPrimitive("GameType")!!.asInt,
-            groupJson.getAsJsonPrimitive("Difficulty")!!.asInt,
+            MatchMode.values()[groupJson.getAsJsonPrimitive("GameType")!!.asInt],
+            Difficulty.values()[groupJson.getAsJsonPrimitive("Difficulty")!!.asInt],
             groupJson.getAsJsonPrimitive("Status")!!.asInt,
             UUID.fromString(groupJson.getAsJsonObject("Owner")!!.getAsJsonPrimitive("ID")!!.asString),
             jsonArrayToUUID(groupJson.getAsJsonArray("Players")!!)
