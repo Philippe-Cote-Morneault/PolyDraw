33a76908f1a75afccccb1b4b8d9676eb21470c8b
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 18:38:12 -0500

    MatchLobbyPresenter will now wait until GroupManager is ready before calling init.

--- Mobile/app/src/main/java/com/log3900/game/lobby/MatchLobbyPresenter.kt
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchLobbyPresenter.kt
@@ -1,20 +1,41 @@
 package com.log3900.game.lobby
 
+import com.log3900.game.group.Group
+import com.log3900.game.group.GroupCreated
 import com.log3900.shared.architecture.Presenter
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class MatchLobbyPresenter : Presenter {
     private var matchLobbyView: MatchLobbyView? = null
+    private var groupManager: GroupManager? = null
 
     constructor(matchLobbyView: MatchLobbyView) {
         this.matchLobbyView = matchLobbyView
+
+        GroupManager.getInstance()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    groupManager = it
+                    init()
+                },
+                {
+
+                }
+            )
     }
+
+    private fun init() {
+
+    }
+
 
     fun onCreateMatchClicked() {
         matchLobbyView?.showMatchCreationDialog()
     }
 
-    fun onCreateMatchDialogPositiveClick() {
-
+    fun onCreateMatch(createdGroup: GroupCreated) {
+        groupManager?.createGroup(createdGroup)
     }
 
     override fun resume() {
