56aa8ec7483b50d833aa40a86bc41a8a986ce930
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-03 19:00:29 -0500

    MatchLobbyFragment now keeps reference to its recyclerview.

--- Mobile/app/src/main/java/com/log3900/game/lobby/MatchLobbyFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchLobbyFragment.kt
@@ -5,18 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.log3900.R
 
 class MatchLobbyFragment : Fragment(), MatchLobbyView {
     private lateinit var matchLobbyPresenter: MatchLobbyPresenter
 
+    // UI
+    private lateinit var matchesRecyclerView: RecyclerView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_match_lobby, container, false)
 
+        setupUiElements(rootView)
 
         matchLobbyPresenter = MatchLobbyPresenter(this)
 
         return rootView
+    }
+
+    private fun setupUiElements(rootView: View) {
+        matchesRecyclerView = rootView.findViewById(R.id.fragment_match_lobby_recycler_view_matches)
     }
 
 }
