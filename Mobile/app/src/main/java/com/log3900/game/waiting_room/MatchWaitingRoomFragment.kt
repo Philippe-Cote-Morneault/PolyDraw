8874bce72e254516f0c0892743deef6cc48791e3
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 11:36:26 -0500

    MatchWaitingRoomFragment will now create presenter when it is created.

--- Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomFragment.kt
@@ -8,8 +8,11 @@ import androidx.fragment.app.Fragment
 import com.log3900.R
 
 class MatchWaitingRoomFragment : Fragment(), MatchWaitingRoomView {
+    private lateinit var matchWaitingRoomPresenter: MatchWaitingRoomPresenter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_match_waiting_room, container, false)
+        
+        matchWaitingRoomPresenter = MatchWaitingRoomPresenter(this)
 
         return rootView
     }
