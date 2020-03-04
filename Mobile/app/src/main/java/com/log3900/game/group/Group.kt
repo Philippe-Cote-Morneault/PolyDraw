f31045c1a2ff3fbe46eb1bb0814a2fed1ca28d48
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 16:22:56 -0500

    Added Difficulty and MatchMode enums in Group.

--- Mobile/app/src/main/java/com/log3900/game/group/Group.kt
+++ Mobile/app/src/main/java/com/log3900/game/group/Group.kt
@@ -1,8 +1,41 @@
 package com.log3900.game.group
 
+import com.log3900.R
 import com.squareup.moshi.Json
 import java.util.*
 import kotlin.collections.ArrayList
+
+enum class Difficulty {
+    EASY,
+    MEDIUM,
+    HARD;
+
+    companion object {
+        fun stringRes(item: Difficulty): Int {
+            when (item) {
+                EASY -> return R.string.difficulty_easy_title
+                MEDIUM -> return R.string.difficulty_medium_title
+                HARD -> return R.string.difficulty_hard_title
+            }
+        }
+    }
+}
+
+enum class MatchMode {
+    FFA,
+    COOP,
+    SOLO;
+
+    companion object {
+        fun stringRes(item: MatchMode): Int {
+            when (item) {
+                FFA -> return R.string.match_mode_ffa_title
+                COOP -> return R.string.match_mode_coop_title
+                SOLO -> return R.string.match_mode_solo_title
+            }
+        }
+    }
+}
 
 class Group(@Json(name = "ID") var ID: UUID, @Json(name = "GroupName") var groupName: String,
             @Json(name = "PlayersMax") var playersMax: Int, @Json(name = "VirtualPlayer") var virtualPlayers: Int,
