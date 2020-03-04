407951aee86756c180360a22d4a311251009e24e
Martin Pouliot <martinp507@gmail.com>
2020-03-04 13:33:08 -0500

    Started implementing some game logic

--- Server/internal/services/match/mode/ffa.go
+++ Server/internal/services/match/mode/ffa.go
@@ -4,23 +4,43 @@ import (
 	"github.com/google/uuid"
 	"gitlab.com/jigsawcorp/log3900/internal/socket"
 	"gitlab.com/jigsawcorp/log3900/model"
+	"gitlab.com/jigsawcorp/log3900/pkg/sliceutils"
+	"math/rand"
+	"time"
 )
 
 //FFA Free for all game mode
 type FFA struct {
 	base
+	order     []int
+	orderPos  int
+	curLap    int
+	lapsTotal int
+	rand      *rand.Rand
+	timeImage int
+	isRunning bool
 }
 
 //Init initialize the game mode
 func (f *FFA) Init(connections []uuid.UUID, info model.Group) {
+
 	f.init(connections, info)
+	f.rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	f.isRunning = true
+
 }
 
 //Start the game mode
 func (f *FFA) Start() {
 	f.waitForPlayers()
+
+	//Generate players positions
+	f.setOrder()
+
 	//We can start the game loop
-	f.GameLoop()
+	for f.isRunning {
+		f.GameLoop()
+	}
 }
 
 //Ready registering that it is ready
@@ -31,9 +51,41 @@ func (f *FFA) Ready(socketID uuid.UUID) {
 //GameLoop method should be called with start
 func (f *FFA) GameLoop() {
 	//Choose a user.
+	curDrawer := f.connections[f.order[f.orderPos]]
+	drawingID := uuid.New()
+	word := ""
+	message := socket.RawMessage{}
+	//TODO find a word
+	message.ParseMessagePack(byte(socket.MessageType.PlayerDrawThis), PlayerDrawThis{
+		Word:      word,
+		Time:      f.timeImage,
+		DrawingID: drawingID.String(),
+	})
+	socket.SendRawMessageToSocketID(message, curDrawer.socketID)
+
+	message.ParseMessagePack(byte(socket.MessageType.PlayerDrawingTurn), PlayerTurnDraw{
+		UserID:    curDrawer.userID.String(),
+		Username:  curDrawer.Username,
+		Time:      f.timeImage,
+		DrawingID: drawingID.String(),
+		Length:    len(word),
+	})
+	f.broadcast(&message)
 	//Make him draw
-	//Keep track of the scores of every user
-	//Check for guessing
+	//TODO register with the drawing service the drawing ID to route to the correct users
+	time.Sleep(time.Millisecond * time.Duration(f.timeImage))
+
+	f.orderPos++
+	if f.orderPos > len(f.connections)-1 {
+		f.orderPos = 0
+		f.curLap++
+
+		//Is the game finished ?
+		if f.curLap > f.lapsTotal-1 {
+			f.isRunning = false
+			return
+		}
+	}
 }
 
 //Disconnect endpoint for when a user exits
@@ -58,7 +110,11 @@ func (f *FFA) Close() {
 
 //GetConnections returns all the socketID of the match
 func (f *FFA) GetConnections() []uuid.UUID {
-	return f.connections
+	connections := make([]uuid.UUID, 0, len(f.connections))
+	for i := range connections {
+		connections = append(connections, f.connections[i].socketID)
+	}
+	return connections
 }
 
 //GetWelcome returns a packet to send to all the players. Presents various details about the game
@@ -72,15 +128,38 @@ func (f *FFA) GetWelcome() socket.RawMessage {
 		})
 	}
 	//TODO parameters ??
+	f.timeImage = 30000
+	f.lapsTotal = 3
 	welcome := ResponseGameInfo{
 		Players:   players,
 		GameType:  0,
-		TimeImage: 30000,
-		Laps:      3,
+		TimeImage: f.timeImage,
+		Laps:      f.lapsTotal,
 		TotalTime: 0,
 	}
 	message := socket.RawMessage{}
 	message.ParseMessagePack(byte(socket.MessageType.GameWelcome), welcome)
 	return message
 
+}
+
+func (f *FFA) setOrder() {
+	choices := make([]int, 0, len(f.connections))
+	f.order = make([]int, 0, len(f.connections))
+	for i := range f.connections {
+		choices[i] = i
+	}
+
+	for i := len(choices) - 1; i <= 0; i-- {
+		choicePos := f.rand.Intn(i)
+		f.order = append(f.order, sliceutils.PopInt(&choices, choicePos))
+	}
+
+	f.orderPos = 0
+
+}
+
+//finish when the match terminates announce winner
+func (f *FFA) finish() {
+	//Send a message to all the players to give them the details of the game and who is the winner
 }
