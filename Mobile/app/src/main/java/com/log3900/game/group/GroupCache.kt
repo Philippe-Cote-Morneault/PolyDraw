e8abf64036d91af519f4c145f1013bf309ca31bc
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 19:38:18 -0500

    Added methods in GroupCache to control data.

--- Mobile/app/src/main/java/com/log3900/game/group/GroupCache.kt
+++ Mobile/app/src/main/java/com/log3900/game/group/GroupCache.kt
@@ -1,5 +1,22 @@
 package com.log3900.game.group
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 class GroupCache {
-    
+    private var groups: ArrayList<Group> = arrayListOf()
+
+    fun addGroup(group: Group) {
+        groups.add(group)
+    }
+
+    fun removeGroup(groupID: UUID) {
+        groups.removeIf {
+            it.ID == groupID
+        }
+    }
+
+    fun getAllGroups(): ArrayList<Group> {
+        return groups
+    }
 }
