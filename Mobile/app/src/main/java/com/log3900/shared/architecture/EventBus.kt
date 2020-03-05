27d741dafe3bfc0786b23ba8ca13826b3c46e47c
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-05 14:17:27 -0500

    Added GroupCreated and GroupDeleted events to EventBus.

--- Mobile/app/src/main/java/com/log3900/shared/architecture/EventBus.kt
+++ Mobile/app/src/main/java/com/log3900/shared/architecture/EventBus.kt
@@ -1,14 +1,20 @@
 package com.log3900.shared.architecture
 
 enum class EventType {
+    // Channel
     ACTIVE_CHANNEL_CHANGED,
     ACTIVE_CHANNEL_MESSAGE_RECEIVED,
     SUBSCRIBED_TO_CHANNEL,
     UNSUBSCRIBED_FROM_CHANNEL,
     CHANNEL_CREATED,
     CHANNEL_DELETED,
+    // Message
     RECEIVED_MESSAGE,
     UNREAD_MESSAGES_CHANGED,
+    // Group
+    GROUP_CREATED,
+    GROUP_DELETED,
+    // Session
     LOGOUT,
 
 }
