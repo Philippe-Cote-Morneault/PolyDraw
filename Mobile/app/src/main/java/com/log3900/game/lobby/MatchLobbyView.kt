0d6d91f8238b6a8f292ac76bf14c21fa15be8d78
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-05 15:09:35 -0500

    MatchLobbyView has method to updated a group.

--- Mobile/app/src/main/java/com/log3900/game/lobby/MatchLobbyView.kt
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchLobbyView.kt
@@ -1,9 +1,12 @@
 package com.log3900.game.lobby
 
 import com.log3900.game.group.Group
+import java.util.*
+import kotlin.collections.ArrayList
 
 interface MatchLobbyView {
     fun showMatchCreationDialog()
     fun setAvailableGroups(groups: ArrayList<Group>)
     fun notifyMatchesChanged()
+    fun groupUpdated(groupID: UUID)
 }
