921a19fd16ab275852bbe124c1168565232a0e6c
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-04 21:55:53 -0500

    Fixed controls of buttons for number pickers.

--- Mobile/app/src/main/java/com/log3900/game/lobby/MatchCreationDialogFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/lobby/MatchCreationDialogFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.textfield.TextInputEditText
@@ -20,11 +21,16 @@ import com.log3900.game.group.GroupCreated
 import com.log3900.game.group.MatchMode
 
 class MatchCreationDialogFragment(var listener: Listener? = null) : DialogFragment() {
+    // UI
     private lateinit var groupNameTextInput: TextInputEditText
     private lateinit var maxPlayersTextInput: EditText
     private lateinit var virtualPlayersTextInput: EditText
     private lateinit var gameTypeSpinner: Spinner
     private lateinit var difficultySpinner: Spinner
+    private lateinit var removeMaxPlayersButton: ImageView
+    private lateinit var addMaxPlayersButton: ImageView
+    private lateinit var removeVirtualPlayersButton: ImageView
+    private lateinit var addVirtualPlayersButton: ImageView
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialogBuilder = AlertDialog.Builder(activity)
@@ -55,6 +61,26 @@ class MatchCreationDialogFragment(var listener: Listener? = null) : DialogFragme
         virtualPlayersTextInput = rootView.findViewById(R.id.dialog_create_match_edit_text_virtual_players)
         gameTypeSpinner = rootView.findViewById(R.id.dialog_create_match_spinner_match_type)
         difficultySpinner = rootView.findViewById(R.id.dialog_create_match_spinner_difficulty)
+        removeMaxPlayersButton = rootView.findViewById(R.id.dialog_create_match_button_remove_max_player)
+        addMaxPlayersButton = rootView.findViewById(R.id.dialog_create_match_button_add_max_player)
+        removeVirtualPlayersButton = rootView.findViewById(R.id.dialog_create_match_button_remove_virtual_player)
+        addVirtualPlayersButton = rootView.findViewById(R.id.dialog_create_match_button_add_virtual_player)
+
+        removeMaxPlayersButton.setOnClickListener {
+            onRemoveMaxPlayersClick()
+        }
+
+        addMaxPlayersButton.setOnClickListener {
+            onAddMaxPlayersClick()
+        }
+
+        removeVirtualPlayersButton.setOnClickListener {
+            onRemoveVirtualPlayersClick()
+        }
+
+        addVirtualPlayersButton.setOnClickListener {
+            onAddVirtualPlayersClick()
+        }
 
         setupSpinners()
     }
@@ -77,6 +103,26 @@ class MatchCreationDialogFragment(var listener: Listener? = null) : DialogFragme
         val difficultyAdapter = ArrayAdapter<String>(context!!, android.R.layout.simple_spinner_item, difficultySpinnerItems)
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         difficultySpinner.adapter = difficultyAdapter
+    }
+
+    private fun onRemoveMaxPlayersClick() {
+        var newCount = maxPlayersTextInput.text.toString().toInt() - 1
+        maxPlayersTextInput.setText(newCount.toString())
+    }
+
+    private fun onAddMaxPlayersClick() {
+        val newCount = maxPlayersTextInput.text.toString().toInt() + 1
+        maxPlayersTextInput.setText(newCount.toString())
+    }
+
+    private fun onRemoveVirtualPlayersClick() {
+        val newCount = virtualPlayersTextInput.text.toString().toInt() - 1
+        virtualPlayersTextInput.setText(newCount.toString())
+    }
+
+    private fun onAddVirtualPlayersClick() {
+        val newCount = virtualPlayersTextInput.text.toString().toInt() + 1
+        virtualPlayersTextInput.setText(newCount.toString())
     }
 
 
