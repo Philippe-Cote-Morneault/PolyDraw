a8b031685cdee1a80ed01cc511fb9176ccbf867e
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-03-05 18:53:38 -0500

    Fixed a bug with an offset for the uuid

--- Server/internal/services/lobby/groups.go
+++ Server/internal/services/lobby/groups.go
@@ -263,9 +263,12 @@ func (g *groups) removeSocketGroup(socketID uuid.UUID, groupID uuid.UUID) {
 
 func (g *groups) safeDeleteGroup(groupDB *model.Group) {
 	//We remove the group
-	message := socket.RawMessage{}
 	uuidBytes, _ := groupDB.ID.MarshalBinary()
-	message.ParseMessage(byte(socket.MessageType.ResponseGroupRemoved), uint16(len(uuidBytes)), uuidBytes)
+	message := socket.RawMessage{
+		MessageType: byte(socket.MessageType.ResponseGroupRemoved),
+		Length:      uint16(len(uuidBytes)),
+		Bytes:       uuidBytes,
+	}
 	//Broadcast a message to all the users in queue
 	for k := range g.queue {
 		go socket.SendRawMessageToSocketID(message, k)
