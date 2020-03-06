7c4d62de61159368a0a74f0c6b94c327b042f02d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-05 20:55:10 -0500

    Added PlayerAdapter.

---
+++ Mobile/app/src/main/java/com/log3900/game/waiting_room/PlayerAdapter.kt
@@ -0,0 +1,24 @@
+package com.log3900.game.waiting_room
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.log3900.R
+import com.log3900.game.group.Player
+
+class MatchAdapter(var players: ArrayList<Player>, var listener: PlayerViewHolder.Listener) : RecyclerView.Adapter<PlayerViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return players.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_match_waiting_room_player, parent, false)
+
+        return PlayerViewHolder(view, listener)
+    }
+
+    override fun onBindViewHolder(holder: PlayerViewHolder, position: Int) {
+        holder.bind(players[position])
+    }
+}
