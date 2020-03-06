92173a61a1a4d78cf7973ab86315fd07d96959c7
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-05 19:18:52 -0500

    WaitingRoomPresenter now handles leave match click.

--- Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomPresenter.kt
+++ Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomPresenter.kt
@@ -1,12 +1,36 @@
 package com.log3900.game.waiting_room
 
+import com.log3900.chat.ChatManager
+import com.log3900.game.group.GroupManager
 import com.log3900.shared.architecture.Presenter
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class MatchWaitingRoomPresenter : Presenter {
     private var matchWaitingRoomView: MatchWaitingRoomView? = null
+    private var groupManager: GroupManager? = null
 
     constructor(matchWaitingRoomView: MatchWaitingRoomView) {
         this.matchWaitingRoomView = matchWaitingRoomView
+
+        GroupManager.getInstance()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    groupManager = it
+                    init()
+                },
+                {
+
+                }
+            )
+    }
+
+    private fun init() {
+
+    }
+
+    fun onLeaveMatchClick() {
+        groupManager?.leaveCurrentGroup()
     }
 
     override fun resume() {
