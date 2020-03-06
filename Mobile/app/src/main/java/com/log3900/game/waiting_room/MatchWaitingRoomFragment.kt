304bac904a9edada36ec63796bab9cafd2b9e53f
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-05 19:14:01 -0500

    Added listener for leave match button.

--- Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/waiting_room/MatchWaitingRoomFragment.kt
@@ -5,17 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import com.log3900.R
 
 class MatchWaitingRoomFragment : Fragment(), MatchWaitingRoomView {
     private var matchWaitingRoomPresenter: MatchWaitingRoomPresenter? = null
 
+    // UI
+    private lateinit var startMatchButton: MaterialButton
+    private lateinit var leaveMatchButton: MaterialButton
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_match_waiting_room, container, false)
+
+        setupUi(rootView)
 
         matchWaitingRoomPresenter = MatchWaitingRoomPresenter(this)
 
         return rootView
+    }
+
+    private fun setupUi(rootView: View) {
+        startMatchButton = rootView.findViewById(R.id.fragment_match_waiting_room_button_start_match)
+        leaveMatchButton = rootView.findViewById(R.id.fragment_match_waiting_room_button_leave_match)
+
+        setupUIListeners()
+    }
+
+    private fun setupUIListeners() {
+        leaveMatchButton.setOnClickListener {
+            matchWaitingRoomPresenter?.onLeaveMatchClick()
+        }
     }
 
     override fun onDestroy() {
