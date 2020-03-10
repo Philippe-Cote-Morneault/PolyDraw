e76cafb83127ac0cf29a735b49ea5de2fb465477
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-10 15:32:14 -0400

    Added WordGuessingView.

---
+++ Mobile/app/src/main/java/com/log3900/game/match/UI/WordGuessingView.kt
@@ -0,0 +1,77 @@
+package com.log3900.game.match.UI
+
+import android.content.Context
+import android.text.InputFilter
+import android.text.InputType
+import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import com.log3900.R
+
+class WordGuessingView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+    private var layout: ConstraintLayout
+    private var editTextContainer: LinearLayout
+    private var letterEditTexts: ArrayList<EditText> = ArrayList()
+
+    init {
+        layout = View.inflate(context, R.layout.view_word_guessing, this) as ConstraintLayout
+        editTextContainer = layout.findViewById(R.id.view_word_guessing_edit_text_container)
+    }
+
+    fun setWordLength(wordLength: Int) {
+        editTextContainer.removeAllViews()
+        letterEditTexts.clear()
+
+        for (i in 0 until wordLength) {
+            val newEditText = generateEditText()
+            letterEditTexts.add(newEditText)
+            editTextContainer.addView(newEditText)
+        }
+    }
+
+    private fun generateEditText(): EditText {
+        val editText = EditText(context)
+        editText.setEms(1)
+        editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+        editText.filters += InputFilter.LengthFilter(1)
+        editText.inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+
+        setTextChangedListener(editText)
+
+        return editText
+    }
+
+    private fun setTextChangedListener(editText: EditText) {
+        editText.doAfterTextChanged {
+            val newValue = it?.toString()
+            if (newValue != null && newValue.isNotEmpty()) {
+                val editTextIndex = letterEditTexts.indexOf(editText)
+
+                if (editTextIndex < letterEditTexts.size - 1) {
+                    letterEditTexts[editTextIndex + 1].requestFocus()
+                }
+            }
+        }
+        editText.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                if (keyCode == KeyEvent.KEYCODE_DEL) {
+                    if (editText.text.isEmpty()) {
+                        val editTextIndex = letterEditTexts.indexOf(editText)
+
+                        if (editTextIndex > 0) {
+                            letterEditTexts[editTextIndex - 1].requestFocus()
+                        }
+                    }
+                }
+            }
+            false
+        }
+    }
+}
