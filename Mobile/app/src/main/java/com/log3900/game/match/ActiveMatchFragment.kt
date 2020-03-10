7823a3966d290d8a8872daa7a514b4115bbb880d
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-10 15:33:20 -0400

    ActiveMatchFragment now sets character length on WordGuessingView.

--- Mobile/app/src/main/java/com/log3900/game/match/ActiveMatchFragment.kt
+++ Mobile/app/src/main/java/com/log3900/game/match/ActiveMatchFragment.kt
@@ -6,13 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.log3900.R
+import com.log3900.game.match.UI.WordGuessingView
 
 class ActiveMatchFragment : Fragment(), ActiveMatchView {
     private var activeMatchPresenter: ActiveMatchPresenter? = null
 
+    // UI
+    private lateinit var guessingView: WordGuessingView
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_active_match, container, false)
+        guessingView = rootView.findViewById(R.id.fragment_active_match_guess_container)
+        guessingView.setWordLength(5)
 
         activeMatchPresenter = ActiveMatchPresenter(this)
 
