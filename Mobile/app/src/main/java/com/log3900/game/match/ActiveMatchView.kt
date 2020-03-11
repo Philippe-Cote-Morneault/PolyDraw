abbbb801c56e30394e493ab3a5e4e32d231fd842
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-11 00:08:12 -0400

    Added more methods to ActiveMatchView interface.

--- Mobile/app/src/main/java/com/log3900/game/match/ActiveMatchView.kt
+++ Mobile/app/src/main/java/com/log3900/game/match/ActiveMatchView.kt
@@ -1,7 +1,11 @@
 package com.log3900.game.match
 
 import com.log3900.game.group.Player
+import java.util.*
+import kotlin.collections.ArrayList
 
 interface ActiveMatchView {
     fun setPlayers(players: ArrayList<Player>)
+    fun setPlayerStatus(playerID: UUID, statusImageRes: Int)
+    fun clearAllPlayerStatusRes()
 }
