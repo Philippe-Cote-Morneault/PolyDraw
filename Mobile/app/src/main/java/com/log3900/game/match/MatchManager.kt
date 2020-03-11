0d4c2acbcce92829a8616f5387c0b3df234abd1a
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-10 22:17:14 -0400

    Added MatchManager.

---
+++ Mobile/app/src/main/java/com/log3900/game/match/MatchManager.kt
@@ -0,0 +1,12 @@
+package com.log3900.game.match
+
+class MatchManager {
+    private var matchRepository: MatchRepository
+
+    init {
+        matchRepository = MatchRepository.instance!!
+    }
+    fun getCurrentMatch(): Match {
+        return matchRepository.getCurrentMatch()!!
+    }
+}
