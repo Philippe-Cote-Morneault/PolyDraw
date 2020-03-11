fe7ad982222c8757b923272e41395cbad3b76867
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-10 21:16:24 -0400

    Create PlayerViewHolder for ActiveMatchFragment.

---
+++ Mobile/app/src/main/java/com/log3900/game/match/PlayerViewHolder.kt
@@ -0,0 +1,53 @@
+package com.log3900.game.match
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.log3900.MainApplication
+import com.log3900.R
+import com.log3900.game.group.Player
+import com.log3900.game.waiting_room.PlayerViewHolder
+import com.log3900.user.UserRepository
+import com.log3900.utils.ui.getAvatarID
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class PlayerViewHolder : RecyclerView.ViewHolder {
+    private lateinit var player: Player
+
+    // UI
+    private var rootView: View
+    private var positionTextView: TextView
+    private var playerChip: Chip
+    private var playerStatusImageView: ImageView
+    private var scoreTextView: TextView
+
+
+    constructor(itemView: View, listener: PlayerViewHolder.Listener? = null) : super(itemView) {
+        rootView = itemView
+        positionTextView = itemView.findViewById(R.id.list_item_active_match_player_text_view_position)
+        playerChip = itemView.findViewById(R.id.list_item_active_match_player_chip_player)
+        playerStatusImageView = itemView.findViewById(R.id.list_item_active_match_player_image_view_status)
+        scoreTextView = itemView.findViewById(R.id.list_item_active_match_player_text_view_score)
+    }
+
+    fun bind(player: Player) {
+        this.player = player
+
+        playerChip.text = player.username
+
+        UserRepository.getInstance().getUser(player.ID)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    playerChip.chipIcon = ResourcesCompat.getDrawable(MainApplication.instance.resources, getAvatarID(it.pictureID), null)
+                },
+                {
+                }
+            )
+    }
+}
