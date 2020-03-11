e1f469ed6e46231b45b81ec5fab7debabe263663
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-10 22:34:23 -0400

    MatchAdapter now has custom player adapter.

--- Mobile/app/src/main/java/com/log3900/utils/format/moshi/MatchAdapter.kt
+++ Mobile/app/src/main/java/com/log3900/utils/format/moshi/MatchAdapter.kt
@@ -1,17 +1,19 @@
 package com.log3900.utils.format.moshi
 
 import android.util.Log
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.log3900.game.group.MatchMode
+import com.log3900.game.group.Player
 import com.log3900.game.match.FFAMatch
 import com.log3900.game.match.Match
 import com.squareup.moshi.FromJson
+import java.util.*
 
 object MatchAdapter {
     @FromJson
     fun fromJson(matchJson: JsonObject): Match {
-        Log.d("POTATO", "Converting match = $matchJson")
-        val players = GroupAdapter().jsonArrayToPlayers(matchJson.getAsJsonArray("Players")!!)
+        val players = jsonArrayToPlayers(matchJson.getAsJsonArray("Players")!!)
         val matchType = MatchMode.values()[matchJson.get("GameType").asInt + 1]
         val timeImage = matchJson.get("TimeImage").asInt
 
@@ -32,5 +34,21 @@ object MatchAdapter {
             timeImage,
             0
         )
+    }
+
+    fun jsonArrayToPlayers(ids: JsonArray): ArrayList<Player> {
+        var arrayList = arrayListOf<Player>()
+
+        ids.forEach {
+            arrayList.add(
+                Player(
+                    UUID.fromString(it.asJsonObject.getAsJsonPrimitive("UserID").asString),
+                    it.asJsonObject.getAsJsonPrimitive("Username").asString,
+                    it.asJsonObject.getAsJsonPrimitive("IsCPU").asBoolean
+                )
+            )
+        }
+
+        return arrayList
     }
 }
