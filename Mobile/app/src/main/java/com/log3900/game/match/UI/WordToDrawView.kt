c30b56f3c065e79c0f8e655898d2848058df27f6
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-15 11:39:59 -0400

    Added WordToDrawView.

---
+++ Mobile/app/src/main/java/com/log3900/game/match/UI/WordToDrawView.kt
@@ -0,0 +1,24 @@
+package com.log3900.game.match.UI
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.log3900.R
+
+class WordToDrawView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+    private var layout: ConstraintLayout
+    private var wordTitleTextView: TextView
+    private var wordTextView: TextView
+
+    init {
+        layout = View.inflate(context, R.layout.view_word_to_draw, this) as ConstraintLayout
+        wordTitleTextView = layout.findViewById(R.id.view_word_to_draw_text_view_word_title)
+        wordTextView = layout.findViewById(R.id.view_word_to_draw_text_view_word)
+    }
+
+    fun setWordToGuess(word: String) {
+        wordTextView.text = word
+    }
+}
