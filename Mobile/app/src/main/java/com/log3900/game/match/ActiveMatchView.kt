62d1d3297b9218509189886d562ef7e524d83fa7
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-03-15 15:13:25 -0400

    ActiveMatchFragment now switches between guessing and drawing view.

--- Mobile/app/src/main/java/com/log3900/game/match/ActiveMatchView.kt
+++ Mobile/app/src/main/java/com/log3900/game/match/ActiveMatchView.kt
@@ -9,7 +9,10 @@ interface ActiveMatchView {
     fun setPlayerStatus(playerID: UUID, statusImageRes: Int)
     fun clearAllPlayerStatusRes()
     fun setWordToGuessLength(length: Int)
+    fun setWordToDraw(word: String)
     fun enableDrawFunctions(enable: Boolean, drawingID: UUID?)
     fun clearCanvas()
     fun setTimeValue(time: String)
+    fun showWordGuessingView()
+    fun showWordToDrawView()
 }
