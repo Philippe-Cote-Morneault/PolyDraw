62721d97bf028359f74f6ebba8b53fa5cc795900
Allan Beddouk <allan.beddouk@polymtl.ca>
2020-03-18 21:19:43 -0400

    Creation de virtualPlayer manager

--- Server/internal/services/virtualplayer/handles.go
+++ Server/internal/services/virtualplayer/handles.go
@@ -3,16 +3,17 @@ package virtualplayer
 import (
 	"log"
 	"math/rand"
+	"sync"
 
 	"github.com/google/uuid"
 	"gitlab.com/jigsawcorp/log3900/internal/socket"
 )
 
-var personnalities []string = []string{"angry", "funny", "mean", "nice", "supportive"}
-var currentPlayers map[uuid.UUID]uuid.UUID //channelID -> playerID
-var currentGames map[uuid.UUID]uuid.UUID   //gameID -> channelID
+var managerInstance *Manager
 
 type virtualPlayerInfos struct {
+	PlayerID          uuid.UUID
+	Username          string
 	Personnality      string
 	DrawingTime       int
 	LastStartLine     byte
@@ -22,8 +23,37 @@ type virtualPlayerInfos struct {
 	LastHintLine      byte
 }
 
+type group struct {
+	id              string
+	playerUsernames []string
+}
+
+// Manager represents a struct that manage all virtual players
+type Manager struct {
+	mutex    sync.Mutex
+	Players  map[uuid.UUID]*virtualPlayerInfos // playerID -> virtualPlayerInfos
+	Channels map[uuid.UUID][]uuid.UUID         //channelID -> []playerID
+	Games    map[uuid.UUID]*group              //channelID -> group
+}
+
+func (m *Manager) init() {
+	m.Players = make(map[uuid.UUID]*virtualPlayerInfos)
+	m.Channels = make(map[uuid.UUID][]uuid.UUID)
+	m.Games = make(map[uuid.UUID]*group)
+}
+
 func (v *VirtualPlayer) addVirtualPlayer(message socket.RawMessageReceived) {
-	log.Println("In addVirtualPlayer")
+
+}
+
+//GetManagerInstance access the Manager instance
+func GetManagerInstance() *Manager {
+	return managerInstance
+}
+
+func hasVirtualPlayer(playerID uuid.UUID) bool {
+	_, ok := managerInstance.Players[playerID]
+	return ok
 }
 
 func (v *VirtualPlayer) kickVirtualPlayer(message socket.RawMessageReceived) {
@@ -31,8 +61,11 @@ func (v *VirtualPlayer) kickVirtualPlayer(message socket.RawMessageReceived) {
 }
 
 func generateVirtualPlayer() *virtualPlayerInfos {
-	return &virtualPlayerInfos{Personnality: personnalities[rand.Intn(len(personnalities))], DrawingTime: -1, LastStartLine: 0,
-		LastEndLine: 0, LastReferenceLine: 0, LastRatioLine: 0, LastHintLine: 0}
+	choice := rand.Intn(5)
+
+	return &virtualPlayerInfos{Personnality: []string{"angry", "funny", "mean", "nice", "supportive"}[choice],
+		Username:    "",
+		DrawingTime: -1, LastStartLine: 0, LastEndLine: 0, LastReferenceLine: 0, LastRatioLine: 0, LastHintLine: 0}
 }
 
 func speak(line string) {
