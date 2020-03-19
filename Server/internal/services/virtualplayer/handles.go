085e4ff2502b4606292096005fe0b4819755aa1a
Allan Beddouk <allan.beddouk@polymtl.ca>
2020-03-19 11:24:44 -0400

    Ajout d'une fonction pour choisir la line du bot

--- Server/internal/services/virtualplayer/handles.go
+++ Server/internal/services/virtualplayer/handles.go
@@ -12,15 +12,14 @@ import (
 var managerInstance *Manager
 
 type virtualPlayerInfos struct {
-	PlayerID          uuid.UUID
-	Username          string
-	Personnality      string
-	DrawingTime       int
-	LastStartLine     byte
-	LastEndLine       byte
-	LastReferenceLine byte
-	LastRatioLine     byte
-	LastHintLine      byte
+	PlayerID     uuid.UUID
+	Personnality string
+	DrawingTime  int
+	// LastStartLine     byte
+	// LastEndLine       byte
+	// LastReferenceLine byte
+	// LastRatioLine     byte
+	// LastHintLine      byte
 }
 
 type group struct {
@@ -56,13 +55,58 @@ func (v *VirtualPlayer) kickVirtualPlayer(message socket.RawMessageReceived) {
 }
 
 func generateVirtualPlayer() *virtualPlayerInfos {
-	choice := rand.Intn(5)
-
-	return &virtualPlayerInfos{Personnality: []string{"angry", "funny", "mean", "nice", "supportive"}[choice],
-		Username:    "",
-		DrawingTime: -1, LastStartLine: 0, LastEndLine: 0, LastReferenceLine: 0, LastRatioLine: 0, LastHintLine: 0}
+	return &virtualPlayerInfos{Personnality: []string{"angry", "funny", "mean", "nice", "supportive"}[rand.Intn(5)],
+		DrawingTime: -1}
 }
 
-func speak(line string) {
+func speak(socketID uuid.UUID, message string) {
+	// sends message by socket
+}
 
+func getLines(interactionType string) *lines {
+	switch interactionType {
+	case "startGame":
+		return iStartGameLines
+	case "endRound":
+		return iEndRoundLines
+	case "hint":
+		return iHintLines
+	default:
+		if rand.Intn(2) == 1 {
+			return iPlayerRefLines
+		}
+		return iWinRatioLines
+	}
+}
+
+func getInteraction(playerID uuid.UUID, interactionType string) string {
+
+	playerInfos, ok := managerInstance.Players[playerID]
+
+	lines := getLines(interactionType)
+
+	if !ok {
+		log.Println("[Virtual Player] -> Can't find bot's id. Aborting interaction...")
+		return ""
+	}
+
+	switch playerInfos.Personnality {
+	case "angry":
+		return lines.Angry[rand.Intn(3)]
+
+	case "funny":
+		return lines.Funny[rand.Intn(3)]
+
+	case "mean":
+		return lines.Mean[rand.Intn(3)]
+
+	case "nice":
+		return lines.Nice[rand.Intn(3)]
+
+	case "supportive":
+		return lines.Supportive[rand.Intn(3)]
+
+	default:
+		return ""
+	}
 }
