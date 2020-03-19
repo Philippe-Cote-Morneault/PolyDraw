e3cb0b5c820612edee733e7e0cd16f2dfee3ec53
Martin Pouliot <martinp507@gmail.com>
2020-03-19 00:18:15 -0400

    Added structs to pass in channels

---
+++ Server/internal/match/channels.go
@@ -0,0 +1,27 @@
+package match
+
+import (
+	"github.com/google/uuid"
+	"gitlab.com/jigsawcorp/log3900/model"
+)
+
+//Player represent a player for in channels communications
+type Player struct {
+	IsCPU    bool
+	Username string
+	ID       uuid.UUID
+}
+
+//RoundStart broadcast when the round start
+type RoundStart struct {
+	MatchID uuid.UUID
+	Drawer  Player
+	Game    *model.Game
+}
+
+//HintRequested broadcast when a user wants a hint
+type HintRequested struct {
+	MatchID  uuid.UUID
+	SocketID uuid.UUID
+	Player   Player
+}
