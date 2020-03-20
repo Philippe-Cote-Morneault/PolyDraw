4a918bfaddaf35efab27b8706c07dd32bf084c88
Pasc <pascal-alexandre.morel@polymtl.ca>
2020-03-20 16:27:23 -0400

    Focus first empty guess textbox

--- Desktop/Projet3/ClientLourd/Views/Controls/Game/EditorZone.xaml.cs
+++ Desktop/Projet3/ClientLourd/Views/Controls/Game/EditorZone.xaml.cs
@@ -285,5 +285,21 @@ namespace ClientLourd.Views.Controls.Game
                 }));
             });
         }
+
+        public void SelectFirstEmptyTextBox(object sender, EventArgs e)
+        {
+            for (int i = 0; i < GuessTextBoxes.Items.Count; i++)
+            {
+                if (Convert.ToChar(GuessTextBoxes.Items[i]) == '\0')
+                {
+                    ContentPresenter c = (ContentPresenter)GuessTextBoxes.ItemContainerGenerator.ContainerFromIndex(i);
+                    TextBox tb = c.ContentTemplate.FindName("textbox", c) as TextBox;
+                    Action focusAction = () => tb.Focus();
+                    Dispatcher.BeginInvoke(focusAction, DispatcherPriority.Render);
+
+                    return;
+                }
+            }
+        }
     }
 }
