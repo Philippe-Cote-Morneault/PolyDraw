f6ec77f11775667aa9d8a28183ed9acc6818ccc2
Pasc <pascal-alexandre.morel@polymtl.ca>
2020-03-21 18:16:26 -0400

    Hide Hearts if mode is FFA

---
+++ Desktop/Projet3/ClientLourd/Utilities/Converters/ModeToVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿using ClientLourd.Utilities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClientLourd.Utilities.Converters
+{
+    class ModeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string parameterString = parameter as string;
+            if (!string.IsNullOrEmpty(parameterString))
+            {
+                string[] parameters = parameterString.Split(new char[] { '|' });
+                foreach(string param in parameters)
+                {
+                    if (Enum.TryParse(param, out GameModes res))
+                    {
+                        if (res == (GameModes)value)
+                        {
+                            return Visibility.Visible;
+                        }
+                    }
+                }
+
+            }
+            return Visibility.Collapsed;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
