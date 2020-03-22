f026c90f76f88129e8467dfb5ddf9ccc856e4693
Martin Pouliot <martin.pouliot@polymtl.ca>
2020-03-22 11:46:29 -0400

    Removed a wrong extension appended to every file

--- Server/internal/datastore/datastore.go
+++ Server/internal/datastore/datastore.go
@@ -23,7 +23,7 @@ func GetFile(key string) (*bufio.Reader, error) {
 
 //PostFile used to post the file name
 func PostFile(reader io.Reader) (string, error) {
-	fileName := GenerateFileKey() + ".svg"
+	fileName := GenerateFileKey()
 	file, err := os.OpenFile(GetPath(fileName), os.O_WRONLY|os.O_CREATE, 0666)
 	defer file.Close()
 	if err != nil {
