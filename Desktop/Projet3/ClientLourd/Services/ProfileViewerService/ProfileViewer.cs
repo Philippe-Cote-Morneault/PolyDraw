8ea83e3ee1ac724f780141ccbe6800e813f89067
Pasc <pascal-alexandre.morel@polymtl.ca>
2020-03-23 18:51:41 -0400

    Fix closable Lobby creation dialog and bind User to public profile dialog

--- Desktop/Projet3/ClientLourd/Services/ProfileViewerService/ProfileViewer.cs
+++ Desktop/Projet3/ClientLourd/Services/ProfileViewerService/ProfileViewer.cs
@@ -2,25 +2,26 @@
 using ClientLourd.Views.Dialogs;
 using MaterialDesignThemes.Wpf;
 using System.Windows.Input;
+using ClientLourd.Models.Bindable;
 
 namespace ClientLourd.Services.ProfileViewerService
 {
     public class ProfileViewer
     {
-        static private RelayCommand<object> _viewPublicProfileCommand;
+        static private RelayCommand<Message> _viewPublicProfileCommand;
 
         static public ICommand ViewPublicProfileCommand
         {
             get
             {
                 return _viewPublicProfileCommand ??
-                       (_viewPublicProfileCommand = new RelayCommand<object>(param => OpenPublicProfile()));
+                       (_viewPublicProfileCommand = new RelayCommand<Message>(param => OpenPublicProfile(param)));
             }
         }
 
-        static private void OpenPublicProfile() 
+        static private void OpenPublicProfile(Message param) 
         {
-            DialogHost.Show(new ClosableErrorDialog("Hi!"), "Default");
+            DialogHost.Show(new PublicProfileDialog(), "Default");
 
         }
 
