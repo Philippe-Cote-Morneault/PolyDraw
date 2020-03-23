0dfa8921c95b14708456a97e7c0262ed7d059012
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2020-03-23 17:44:19 -0400

    Reset bearer token on logout

--- Mobile/app/src/main/java/com/log3900/login/BearerTokenManager.kt
+++ Mobile/app/src/main/java/com/log3900/login/BearerTokenManager.kt
@@ -5,6 +5,11 @@ import android.content.SharedPreferences
 import android.util.Log
 import com.log3900.MainApplication
 import com.log3900.R
+import com.log3900.shared.architecture.EventType
+import com.log3900.shared.architecture.MessageEvent
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 object BearerTokenManager {
     private val sharedPrefs: SharedPreferences = with (MainApplication.instance) {
@@ -12,6 +17,9 @@ object BearerTokenManager {
     }
     private val bearerPrefKey = MainApplication.instance.resources.getString(R.string.preference_file_bearer_token_key)
 
+    init {
+        EventBus.getDefault().register(this)
+    }
 
     fun getBearer(): String? = sharedPrefs.getString(bearerPrefKey, null)
 
@@ -28,6 +36,15 @@ object BearerTokenManager {
         with(sharedPrefs.edit()) {
             putString(bearerPrefKey, null)
             commit()
+        }
+        Log.d("BEAR_MAN", "Bearer token is now -> ${getBearer()}")
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onEvent(event: MessageEvent) {
+        when (event.type) {
+            EventType.LOGOUT -> resetBearer()
+            else -> return
         }
     }
 }
