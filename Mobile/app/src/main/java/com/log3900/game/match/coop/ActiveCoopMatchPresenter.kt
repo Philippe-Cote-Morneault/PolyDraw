2465f30adbc575514eef0a8e319de47dbdb95901
Philippe.Cote-Morneault <philippe.cote-morneault@polymtl.ca>
2020-04-03 18:03:13 -0400

    Coop match is now identical to desktop.

--- Mobile/app/src/main/java/com/log3900/game/match/coop/ActiveCoopMatchPresenter.kt
+++ Mobile/app/src/main/java/com/log3900/game/match/coop/ActiveCoopMatchPresenter.kt
@@ -1,13 +1,13 @@
 package com.log3900.game.match.coop
 
 import android.os.Handler
-import android.util.Log
 import com.log3900.MainApplication
 import com.log3900.R
 import com.log3900.game.match.*
-import com.log3900.game.match.ffa.ActiveFFAMatchView
+import com.log3900.settings.sound.SoundManager
 import com.log3900.shared.architecture.EventType
 import com.log3900.shared.architecture.MessageEvent
+import com.log3900.user.account.AccountRepository
 import com.log3900.utils.format.DateFormatter
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -24,7 +24,7 @@ class ActiveCoopMatchPresenter : ActiveMatchPresenter {
 
         activeCoopMatchView.setTeamScore((MainApplication.instance.getContext().resources.getString(R.string.team_score_title, 0)))
         activeCoopMatchView.setRemainingLives(coopMatchManager.getCurrentMatch().lives)
-        activeCoopMatchView.enableHintButton(true)
+        activeCoopMatchView.showHintButton(true)
 
         val drawer = coopMatchManager.getCurrentMatch().players.find { it.isCPU }
         activeCoopMatchView.setDrawer(drawer!!)
@@ -34,12 +34,10 @@ class ActiveCoopMatchPresenter : ActiveMatchPresenter {
 
     override fun onMatchSynchronisation(synchronisation: Synchronisation) {
         super.onMatchSynchronisation(synchronisation)
+        var currentMatch = coopMatchManager.getCurrentMatch()
+        currentMatch.lives = synchronisation.lives!!
         activeCoopMatchView?.setRemainingLives(synchronisation.lives!!)
         activeCoopMatchView?.setTeamScore(MainApplication.instance.getContext().resources.getString(R.string.team_score_title,synchronisation.players[0].second))
-    }
-
-    override fun onGuessedWordWrong() {
-        super.onGuessedWordWrong()
     }
 
     override fun onRoundEnded(roundEnded: RoundEnded) {
@@ -48,6 +46,62 @@ class ActiveCoopMatchPresenter : ActiveMatchPresenter {
         roundEnded.players.forEach {
             updatePlayerScore(it.userID, it.points, it.newPoints)
         }
+
+        if (coopMatchManager.getCurrentMatch().lives == 0) {
+            activeCoopMatchView?.setCanvasMessage(MainApplication.instance.getContext().getString(R.string.the_word_was, roundEnded.word))
+            activeCoopMatchView?.showCanvasMessageView(true)
+        }
+
+        Handler().postDelayed({
+            activeCoopMatchView?.hideCanvas()
+            activeCoopMatchView?.hideRoundEndInfoView()
+            activeCoopMatchView?.showCanvasMessageView(false)
+            Handler().postDelayed({
+                activeCoopMatchView?.clearCanvas()
+                activeCoopMatchView?.showCanvas()
+            }, 500)
+        }, 2000)
+    }
+
+    override fun onTeamateGuessedWordProperly(teamateGuessedWordProperly: TeamateGuessedWordProperly) {
+        super.onTeamateGuessedWordProperly(teamateGuessedWordProperly)
+        if (teamateGuessedWordProperly.userID == AccountRepository.getInstance().getAccount().ID) {
+            activeCoopMatchView?.animateWordGuessedRight()
+            activeCoopMatchView?.setCanvasMessage(MainApplication.instance.getContext().getString(R.string.you_guessed_correctly))
+            activeCoopMatchView?.showCanvasMessageView(true)
+        } else {
+            activeCoopMatchView?.enableGuessingView(false)
+            activeCoopMatchView?.setCanvasMessage(MainApplication.instance.getContext().getString(R.string.coop_teamate_guessed_correctly, teamateGuessedWordProperly.username, teamateGuessedWordProperly.word))
+            activeCoopMatchView?.showCanvasMessageView(true)
+        }
+    }
+
+    override fun onGuessedWordWrong() {
+        if (coopMatchManager.getCurrentMatch().lives > 1) {
+            activeCoopMatchView?.setCanvasMessage(MainApplication.instance.getContext().getString(R.string.try_again))
+            activeCoopMatchView?.showCanvasMessageView(true)
+        }
+        activeCoopMatchView?.enableGuessingView(false)
+        activeCoopMatchView?.animateWordGuessedWrong()
+        SoundManager.playSoundEffect(MainApplication.instance.getContext(), R.raw.sound_effect_word_guessed_wrong)
+        Handler().postDelayed({
+            activeCoopMatchView?.showCanvasMessageView(false)
+            if (canEnableGuessingView) {
+                activeCoopMatchView?.enableGuessingView(true)
+            }
+            activeCoopMatchView?.clearGuessingViewText()
+        }, 2000)
+    }
+
+
+    override fun onMatchEnded(matchEnded: MatchEnded) {
+        activeCoopMatchView?.showConfetti()
+        val score = matchEnded.players.find { it.userID == AccountRepository.getInstance().getAccount().ID }!!.points
+        activeCoopMatchView?.setCanvasMessage(MainApplication.instance.getContext().getString(R.string.solo_match_is_over_message, score))
+        activeCoopMatchView?.showCanvasMessageView(true)
+        Handler().postDelayed({
+            activeCoopMatchView?.showCanvasMessageView(false)
+        }, 2000)
     }
 
     private fun onCheckpoint(checkPoint: CheckPoint) {
