9034602539aaeda157cb68bb02a325eb365f71b0
cedric <cedric.tessier@polymtl.ca>
2020-04-09 17:02:03 -0400

    Fixed canvas movement

---
+++ Desktop/Projet3/ClientLourd/Utilities/Converters/BoolToHiddenConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClientLourd.Utilities.Converters
+{
+    public class BoolToHiddenConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (bool)value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
